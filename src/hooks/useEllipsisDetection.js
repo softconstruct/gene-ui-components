24fc5e7e34a1d37ac75b2b0093eab4c4251b4dd9
Galust <galust.gabrielyan@softconstruct.com>
2024-02-13 18:43:21 +0400

    feat(useEllipsisDetection): add detect multiline ellipsis functionality

--- src/hooks/useEllipsisDetection.js
+++ src/hooks/useEllipsisDetection.js
@@ -1,13 +1,15 @@
 import { useEffect, useState } from 'react';
 
+const EQUAL_HEIGHT_DIFF = 3;
+
 const useEllipsisDetection = (ref, externalDependencies = []) => {
     const [isTruncated, setIsTruncated] = useState(false);
 
     useEffect(() => {
         const handleResize = () => {
             if (!ref.current) return;
-            const { scrollWidth, clientWidth } = ref.current;
-            setIsTruncated(scrollWidth > clientWidth);
+            const { scrollWidth, clientWidth, scrollHeight, clientHeight } = ref.current;
+            setIsTruncated(scrollWidth > clientWidth || scrollHeight > clientHeight + EQUAL_HEIGHT_DIFF);
         };
 
         window.addEventListener('resize', handleResize);
@@ -15,7 +17,14 @@ const useEllipsisDetection = (ref, externalDependencies = []) => {
         handleResize();
 
         return () => window.removeEventListener('resize', handleResize);
-    }, [ref, ref?.current?.scrollWidth, ref?.current?.clientWidth, ...externalDependencies]);
+    }, [
+        ref,
+        ref?.current?.scrollWidth,
+        ref?.current?.clientWidth,
+        ref?.current?.scrollHeight,
+        ref?.current?.clientHeight,
+        ...externalDependencies
+    ]);
 
     return isTruncated;
 };
