457fd1e0b7bd0cef4227c9f12ee6c2db58ed7c4c
Galust <galust.gabrielyan@softconstruct.com>
2024-03-04 14:08:11 +0400

    fix(useEllipsisDetection): add debounce for window resize

--- src/hooks/useEllipsisDetection.js
+++ src/hooks/useEllipsisDetection.js
@@ -6,17 +6,26 @@ const useEllipsisDetection = (ref, externalDependencies = []) => {
     const [isTruncated, setIsTruncated] = useState(false);
 
     useEffect(() => {
+        let timeoutId;
         const handleResize = () => {
             if (!ref.current) return;
             const { scrollWidth, clientWidth, scrollHeight, clientHeight } = ref.current;
             setIsTruncated(scrollWidth > clientWidth || scrollHeight > clientHeight + EQUAL_HEIGHT_DIFF);
         };
 
-        window.addEventListener('resize', handleResize);
+        const debounceResize = () => {
+            clearTimeout(timeoutId);
+            timeoutId = setTimeout(handleResize, 100);
+        };
+
+        window.addEventListener('resize', debounceResize);
 
         handleResize();
 
-        return () => window.removeEventListener('resize', handleResize);
+        return () => {
+            window.removeEventListener('resize', debounceResize);
+            clearTimeout(timeoutId);
+        };
     }, [
         ref,
         ref?.current?.scrollWidth,
