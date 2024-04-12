df5f1a56fc1479c546364db9c7cb7a89043e730e
Galust <galust.gabrielyan@softconstruct.com>
2024-04-12 16:45:15 +0400

    refactor(BusyLoader): test

--- src/lib/atoms/BusyLoader/BusyLoader.test.tsx
+++ src/lib/atoms/BusyLoader/BusyLoader.test.tsx
@@ -9,41 +9,41 @@ import BusyLoader from './index';
 //Types
 import { IBusyLoaderProps } from './index';
 
-describe('BusyLoader Component', () => {
+describe('BusyLoader ', () => {
     let setup: ReactWrapper<IBusyLoaderProps>;
     beforeEach(() => (setup = mount(<BusyLoader />)));
 
     it('exists', () => expect(setup.exists()).toBeTruthy());
 
-    it('"className" pass correct', () => {
+    it('renders className prop correctly', () => {
         const className = 'test-class';
         const wrapper = setup.setProps({ className });
 
         expect(wrapper.hasClass(className)).toBeTruthy();
     });
 
-    it('"children" and "isBusy" props pass correct', () => {
+    it('renders children and isBusy prop correctly', () => {
         const children = <span>test</span>;
         const wrapper = setup.setProps({ children, isBusy: false });
 
         expect(wrapper.contains('test')).toBeTruthy();
     });
 
-    it('"loadingText" prop pass correct', () => {
+    it('renders loadingText prop correctly', () => {
         const loadingText = 'loading text';
         const wrapper = setup.setProps({ loadingText });
 
         expect(wrapper.contains(loadingText)).toBeTruthy();
     });
 
-    it.each<IBusyLoaderProps['spinnerSize']>(['small', 'medium', 'big'])('%s size passing correct', (spinnerSize) => {
+    it.each<IBusyLoaderProps['spinnerSize']>(['small', 'medium', 'big'])('should have %s size', (spinnerSize) => {
         const wrapper = setup.setProps({ spinnerSize });
 
         expect(wrapper.props().spinnerSize).toBe(spinnerSize);
         expect(wrapper.find('.bc-icon-loader').hasClass(`s-${spinnerSize}`)).toBeTruthy();
     });
 
-    it.each<IBusyLoaderProps['type']>(['spinner', 'bubbles', 'bar'])('%s type passing correct', (type) => {
+    it.each<IBusyLoaderProps['type']>(['spinner', 'bubbles', 'bar'])('should have %s type', (type) => {
         const wrapper = setup.setProps({ type });
 
         expect(wrapper.props().type).toBe(type);
