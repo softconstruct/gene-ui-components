a7df8f52a96fd7cd80ce4549f3fe0f8c0b7815df
Galust <galust.gabrielyan@softconstruct.com>
2024-04-12 13:41:49 +0400

    refactor(Badge): test

--- src/lib/atoms/Badge/Badge.test.tsx
+++ src/lib/atoms/Badge/Badge.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { shallow, ShallowWrapper, mount } from 'enzyme';
+import { mount, ReactWrapper } from 'enzyme';
 
 // Components
 import Badge from './index';
@@ -7,56 +7,59 @@ import Badge from './index';
 // Types
 import { IBadgeProps } from './index';
 
-const shallowComponent = (props?: IBadgeProps): ShallowWrapper => {
-    return shallow(<Badge {...props} />);
-};
-
 describe('Badge Component', () => {
-    it('renders without crashing', () => {
-        const wrapper = shallowComponent();
-        expect(wrapper.exists()).toBeTruthy();
+    let setup: ReactWrapper<IBadgeProps>;
+    const count = 100;
+
+    beforeEach(() => (setup = mount(<Badge />)));
+
+    it('exists', () => expect(setup.exists()).toBeTruthy());
+
+    it('"className" pass correct', () => {
+        const className = 'test-class';
+        const wrapper = setup.setProps({ className });
+
+        expect(wrapper.hasClass(className)).toBeTruthy();
     });
 
-    it('className passing correct', () => {
-        const component = mount(<Badge className={'testName'} />);
-        expect(component.props().className).toEqual('testName');
+    it('"count" prop pass correct', () => {
+        const wrapper = setup.setProps({ count });
+
+        expect(wrapper.contains(count)).toBeTruthy();
     });
 
-    it('children renders without crashing', () => {
+    it('"maxCount" prop pass correct', () => {
+        const maxCount = 99;
+        const wrapper = setup.setProps({ count, maxCount });
+
+        expect(wrapper.contains(`${maxCount}+`)).toBeTruthy();
+    });
+
+    it('"children" prop pass correct', () => {
         const children = <span>test</span>;
-        const component = shallow(<Badge>{children}</Badge>);
-        expect(component.contains(children)).toBeTruthy();
+        const wrapper = setup.setProps({ children });
+
+        expect(wrapper.contains('test')).toBeTruthy();
     });
 
-    it('maxCount passing correct', () => {
-        const component = mount(<Badge maxCount={33} />);
-        expect(component.props().maxCount).toEqual(33);
+    it('"dot" pass correct', () => {
+        const dot = true;
+        const wrapper = setup.setProps({ dot, count });
+
+        expect(wrapper.contains(count)).toBeFalsy();
     });
 
-    it('count passing correct', () => {
-        const component = mount(<Badge count={33} />);
-        expect(component.props().count).toEqual(33);
+    it.each<IBadgeProps['size']>(['big', 'medium', 'default', 'huge'])('%s size passing correct', (size) => {
+        const wrapper = setup.setProps({ size, count });
+
+        expect(wrapper.props().size).toBe(size);
+        expect(wrapper.find('.badge__content').hasClass(`badge__content-${size}`)).toBeTruthy();
     });
 
-    it('dot passing correct', () => {
-        const component = mount(<Badge dot />);
-        expect(component.props().dot).toEqual(true);
-    });
+    it.each<IBadgeProps['color']>(['danger', 'primary'])('%s color passing correct', (color) => {
+        const wrapper = setup.setProps({ color, count });
 
-    it('restProps renders without crashing', () => {
-        const component = shallow(<Badge data-test="test" />);
-        expect(component.find(`[data-test='test']`)).toHaveLength(1);
-    });
-
-    it.each(['big', 'medium', 'default', 'huge'])('%s size passing correct', (size) => {
-        const key = size as Pick<IBadgeProps, 'size'>['size'];
-        const component = mount(<Badge size={key} />);
-        expect(component.props().size).toEqual(key);
-    });
-
-    it.each(['danger', 'primary'])('%s color passing correct', (color) => {
-        const key = color as Pick<IBadgeProps, 'color'>['color'];
-        const component = mount(<Badge color={key} />);
-        expect(component.props().color).toEqual(key);
+        expect(wrapper.props().color).toBe(color);
+        expect(wrapper.find('.badge__content').hasClass(`badge__content-${color}`)).toBeTruthy();
     });
 });
