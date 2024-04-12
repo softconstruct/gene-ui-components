ca9636be6a79695809f2af4cb4be014d7ca6d949
Galust <galust.gabrielyan@softconstruct.com>
2024-04-12 16:50:29 +0400

    refactor(Badge): test

--- src/lib/atoms/Badge/Badge.test.tsx
+++ src/lib/atoms/Badge/Badge.test.tsx
@@ -7,56 +7,56 @@ import Badge from './index';
 // Types
 import { IBadgeProps } from './index';
 
-describe('Badge Component', () => {
+describe('Badge ', () => {
     let setup: ReactWrapper<IBadgeProps>;
     const count = 100;
 
     beforeEach(() => (setup = mount(<Badge />)));
 
-    it('exists', () => expect(setup.exists()).toBeTruthy());
+    it('renders without crashing', () => expect(setup.exists()).toBeTruthy());
 
-    it('"className" pass correct', () => {
+    it('renders className prop correctly', () => {
         const className = 'test-class';
         const wrapper = setup.setProps({ className });
 
         expect(wrapper.hasClass(className)).toBeTruthy();
     });
 
-    it('"count" prop pass correct', () => {
+    it('renders count prop correctly', () => {
         const wrapper = setup.setProps({ count });
 
         expect(wrapper.contains(count)).toBeTruthy();
     });
 
-    it('"maxCount" prop pass correct', () => {
+    it('renders maxCount prop correctly', () => {
         const maxCount = 99;
         const wrapper = setup.setProps({ count, maxCount });
 
         expect(wrapper.contains(`${maxCount}+`)).toBeTruthy();
     });
 
-    it('"children" prop pass correct', () => {
+    it('renders children prop correctly', () => {
         const children = <span>test</span>;
         const wrapper = setup.setProps({ children });
 
         expect(wrapper.contains('test')).toBeTruthy();
     });
 
-    it('"dot" pass correct', () => {
+    it('renders dot prop correctly', () => {
         const dot = true;
         const wrapper = setup.setProps({ dot, count });
 
         expect(wrapper.contains(count)).toBeFalsy();
     });
 
-    it.each<IBadgeProps['size']>(['big', 'medium', 'default', 'huge'])('%s size passing correct', (size) => {
+    it.each<IBadgeProps['size']>(['big', 'medium', 'default', 'huge'])('should have %s size', (size) => {
         const wrapper = setup.setProps({ size, count });
 
         expect(wrapper.props().size).toBe(size);
         expect(wrapper.find('.badge__content').hasClass(`badge__content-${size}`)).toBeTruthy();
     });
 
-    it.each<IBadgeProps['color']>(['danger', 'primary'])('%s color passing correct', (color) => {
+    it.each<IBadgeProps['color']>(['danger', 'primary'])('should have %s color', (color) => {
         const wrapper = setup.setProps({ color, count });
 
         expect(wrapper.props().color).toBe(color);
