132da9871f8625b4794f0bd33fcc49fba75b3917
Hamik Hambardzumyan <hamikhambardzumyan25@gmail.com>
2024-06-14 12:20:47 +0400

    test(Tooltip): add unmount after each test to proof that component was destroyed before next case

--- src/lib/molecules/Tooltip/Tooltip.test.tsx
+++ src/lib/molecules/Tooltip/Tooltip.test.tsx
@@ -12,11 +12,16 @@ describe('Tooltip', () => {
         setup = mount(<Tooltip children={<div>z</div>} />, { wrappingComponent: GeneUIProvider });
     });
 
+    afterEach(() => {
+        setup.unmount();
+    });
+
     test('checking if a component exists', async () => {
         expect(setup.exists()).toBeTruthy();
     });
 
     test.each<ITooltipProps['size']>(['default', 'small'])('check with props %p', async (size) => {
+        setup.setProps({ size, alwaysShow: true });
         expect(document.querySelector(`.s-${size}`)).toBeDefined();
     });
 });
