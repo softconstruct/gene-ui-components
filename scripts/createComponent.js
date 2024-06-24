8bc9ea0904b7a7daa85d0a29faf8cdd45a186007
Hamik Hambardzumyan <hamik.hambardzumyan@softconstruct.com>
2024-06-24 19:22:08 +0400

    refactor(CLI): create-component command

--- scripts/createComponent.js
+++ scripts/createComponent.js
@@ -7,9 +7,9 @@ import process from 'process';
 import ora from 'ora';
 import prettier from 'prettier';
 
-const firstLetterChange = (str, toUpperCase = true) => {
-    const firstChar = str.charAt(0);
-    return (toUpperCase ? firstChar.toUpperCase() : firstChar.toLowerCase()) + str.slice(1);
+const firstLetterCase = (str = '', toUpperCase = true) => {
+    const [firstChar, ...remainingChars] = str;
+    return (toUpperCase ? firstChar.toUpperCase() : firstChar.toLowerCase()) + remainingChars.join('');
 };
 
 const spinner = ora({
@@ -18,22 +18,14 @@ const spinner = ora({
     fail: 'Something went wrong please see errors bellow!'
 });
 
-const comments = {
-    STORY_CMP_IMPORTS_START: '/** Start components imports */',
-    STORY_CMP_IMPORTS_END: '/** End components imports */',
-    ADD_STORY_START: '/** Start stories adding */',
-    ADD_STORY_END: '/** End stories adding */'
-};
-
 const messages = {
     ERROR_NAME_EMPTY: chalk.white.redBright.bold("error: Component's name can't be empty!"),
     ERROR_NAME_IS_NOT_CORRECT: chalk.white.redBright.bold("error: Component's name should start with upper case!"),
+    ERROR_PROPS_IS_NOT_CORRECT: chalk.white.redBright.bold("error: Component's props should not contain any symbol!"),
     ERROR_COMPONENT_EXISTS: chalk.white.redBright.bold('error: Component with this name already exists.'),
     ERROR_DUPLICATE_NAME: (value) => chalk.white.redBright.bold(`error: ${value} names should be unique.`),
-    SUCCESS: (componentName, isStory) =>
-        chalk.white.green.bold(
-            `success: The ${componentName} ${isStory ? 'story' : 'component'} is successfully created!`
-        ),
+    SUCCESS: (componentName) =>
+        chalk.white.green.bold(`success: The ${componentName} component is successfully created!`),
     ERROR: (error) => chalk.white.redBright.bold(`error: ${error}.`)
 };
 
@@ -45,42 +37,43 @@ const generateCmpTemplate = ({ name, description, props, isWithForwardRef }) => 
     const InterfaceName = `I${name}Props`;
 
     return `
-    import React${isWithForwardRef ? ', { forwardRef }' : ', { FC }'} from 'react';
+        import React${isWithForwardRef ? ', { forwardRef }' : ', { FC }'} from 'react';
 
-    // Styles
-    import 'src/assets/styles/globalStyling.scss';
-    import './${name}.scss';
-    
-    interface ${InterfaceName} {
-     ${
-         props.length
-             ? `${[...props].map(
-                   (prop) => `
-             /**
-                * ${prop} description
-               */
-                ${prop}?: unknown`
-               )}`
-             : `// fill ${name} component props interface`
-     }
-    }
+        // Styles
+        import './${name}.scss';
+        
+        interface ${InterfaceName} {
+            ${
+                props.length
+                    ? `${props.map(
+                          (prop) => `
+                                /**
+                                * ${prop} description
+                                */
+                                ${prop}?: unknown`
+                      )}`
+                    : `// fill ${name} component props interface`
+            }
+        }
 
-    ${description ? `/** \n* ${description}\n*/` : ''}
-    
-    const ${name}${!isWithForwardRef ? `: FC<I${name}Props>` : ''} = ${
+        ${description ? `/** \n* ${description}\n*/` : ''}
+        const ${name}${!isWithForwardRef ? `: FC<${InterfaceName}>` : ''} = ${
         isWithForwardRef ? `forwardRef<unknown , ${InterfaceName}>((` : '('
-    }${props.length ? `{${[...props]}}` : 'props'}
-    ${isWithForwardRef ? ', ref' : ''}) => {
-        return <div className="${firstLetterChange(name, false)}">
-            ${name}
-        </div>
-    }${isWithForwardRef ? ')' : ''};
+    }${props.length ? `{${[...props]}} ${isWithForwardRef ? `:${  InterfaceName}` : ''}` : 'props'}
+        ${isWithForwardRef ? ', ref' : ''}) => {
+            return <div className="${firstLetterCase(name, false)}">
+                ${name}
+            </div>
+        }${isWithForwardRef ? ')' : ''};
 
-    export { I${name}Props, ${name} as default };
-`;
+        export { ${InterfaceName}, ${name} as default };
+    `;
 };
 
-const generateCmpStoryTemplate = ({ name, level, props }) => `
+const generateCmpStoryTemplate = ({ name, level, props }) => {
+    const InterfaceName = `I${name}Props`;
+
+    return `
         import React, { FC } from 'react';
         import { Meta } from '@storybook/react';
         
@@ -88,17 +81,17 @@ const generateCmpStoryTemplate = ({ name, level, props }) => `
         import { args, propCategory } from '../../../../stories/assets/storybook.globals';
         
         // Components
-        import ${name}, { I${name}Props } from './index';
+        import ${name}, { ${InterfaceName} } from './index';
         
         const meta: Meta<typeof ${name}> = {
-            title: '${firstLetterChange(level)}/${name}',
+            title: '${firstLetterCase(level)}/${name}',
             component: ${name},
             argTypes: {
                  ${
                      props.length
-                         ? `${[...props].map(
+                         ? `${props.map(
                                (prop) => `
-                         ${prop}: args({ control: false, ...propCategory.appearance })`
+                         ${prop}: args({ control: false, ...propCategory.others })`
                            )}`
                          : `// fill ${name} component argTypes`
                  }
@@ -113,18 +106,46 @@ const generateCmpStoryTemplate = ({ name, level, props }) => `
                           )}`
                         : `// fill ${name} component args`
                 }
-            } as I${name}Props
+            } as ${InterfaceName}
         };
         
         export default meta;
         
-        const Template: FC<I${name}Props> = (args) => <${name} {...args} />;
+        const Template: FC<${InterfaceName}> = (args) => <${name} {...args} />;
         
         export const Default = Template.bind({});
         
-        Default.args = {} as I${name}Props
-        
+        Default.args = {} as ${InterfaceName};
         `;
+};
+
+const generateCmpTestTemplate = ({ name, portal }) => {
+    const beforeEach = portal
+        ? `beforeEach(() => (setup = mount(<${name} />, { wrappingComponent: GeneUIProvider })));`
+        : `beforeEach(() => (setup = mount(<${name} />)));`;
+
+    const InterfaceName = `I${name}Props`;
+
+    return `
+            import React from 'react';
+            import { ReactWrapper, mount } from 'enzyme';
+
+            // Components
+            import ${name}, { ${InterfaceName} } from './index';
+            ${portal ? `import GeneUIProvider from '../../providers/GeneUIProvider';` : ''}
+            
+            describe('${name} ', () => {
+                let setup: ReactWrapper<${InterfaceName}>;
+                ${beforeEach}
+
+                it('renders without crashing', () => {
+                    expect(setup.exists()).toBeTruthy();
+                });
+
+                // Your tests here
+            });
+        `;
+};
 
 const init = () => {
     console.log(
@@ -151,15 +172,16 @@ const askQuestions = () => {
             type: 'input',
             message: 'Please enter the component name: ',
             prefix: '[?]',
-            filter: (inputValue) =>
-                // Todo
-                inputValue.trim(),
+            filter: (inputValue) => inputValue.replace(/\s/g, ''),
             validate: async (componentName) => {
-                if (typeof componentName !== 'string' || componentName.trim() === '') {
+                if (componentName === '') {
                     console.log(`\n${messages.ERROR_NAME_EMPTY}`);
                     return false;
                 }
-                if (componentName[0].toUpperCase() !== componentName[0]) {
+
+                const [firstCharOfName] = componentName;
+
+                if (firstCharOfName.toUpperCase() !== firstCharOfName) {
                     console.log(`\n${messages.ERROR_NAME_IS_NOT_CORRECT}`);
                     return false;
                 }
@@ -168,10 +190,7 @@ const askQuestions = () => {
                 const molecules = await fs.readdir(path.join(__dirname, ...pathToLib, 'molecules'));
                 const organisms = await fs.readdir(path.join(__dirname, ...pathToLib, 'organisms'));
 
-                const components = atoms
-                    .filter((atom) => !atom.startsWith('index.'))
-                    .concat(molecules.filter((molecule) => !molecule.startsWith('index.')))
-                    .concat(organisms.filter((organism) => !organism.startsWith('index.')));
+                const components = [...atoms, ...molecules, ...organisms];
 
                 for (let i = 0; i < components.length; i++) {
                     if (components[i].toLowerCase() === componentName.toLowerCase()) {
@@ -190,14 +209,22 @@ const askQuestions = () => {
                 "Please enter props of the component separated by ',' in case you don't know yet what props you need leave the input empty: ",
             prefix: '[?]',
             filter: (value) => (value ? value.split(',').map((prop) => prop.trim()) : []),
-            validate: async (componentProps) => {
+            validate: (componentProps) => {
                 const propDict = {};
+
                 for (let i = 0; i < componentProps.length; i++) {
                     const key = componentProps[i];
+
                     if (propDict.hasOwnProperty(key)) {
                         console.log(`\n${messages.ERROR_DUPLICATE_NAME('Props')}`);
                         return false;
                     }
+
+                    if (!/^[a-zA-Z0-9_]+$/.test(key)) {
+                        console.log(`\n${messages.ERROR_PROPS_IS_NOT_CORRECT}`);
+                        return false;
+                    }
+
                     propDict[key] = key;
                 }
 
@@ -211,7 +238,7 @@ const askQuestions = () => {
                 "If you need extra .ts files in the component folder type files names separated by ',' else leave the input empty: ",
             prefix: '[?]',
             filter: (value) => (value ? value.split(',').map((prop) => prop.trim()) : []),
-            validate: async (files) => {
+            validate: (files) => {
                 const propDict = {};
                 for (let i = 0; i < files.length; i++) {
                     const key = files[i];
@@ -239,7 +266,7 @@ const askQuestions = () => {
             prefix: '[?]',
             message: 'Is this component with portal: ',
             choices: ['Yes', 'No'],
-            filter: (value) => value.toLowerCase()
+            filter: (value) => value === 'Yes'
         },
         {
             name: 'description',
@@ -268,21 +295,22 @@ const createComponentFiles = async ({ level, name, files, ...restData }) => {
         const componentPath = path.join(`${cmpDir}`, `${name}.tsx`);
         const componentFormattedData = prettier.format(srcCode, { ...prettierConfig, parser: 'typescript' });
 
-        // Create index.js file with code
+        // Create index.tsx file with code
         await fs.appendFile(componentPath, componentFormattedData);
 
-        // Create scss file for the component
+        // Create SCSS file for the component
         const scssContent = `
-        @import "src/assets/styles/variables";
         
-        .${firstLetterChange(name, false)}{
-            //your styles here
+        .${firstLetterCase(name, false)} {
+            // Your styles here
+            color: var(--guit-ref-color-magenta-500base);
+            font-size: var(--guit-sem-font-caption-large-medium-font-size);
         }`;
         const scssPath = path.join(`${cmpDir}`, `${name}.scss`);
         const scssFormattedData = prettier.format(scssContent, { ...prettierConfig, parser: 'scss' });
         await fs.appendFile(scssPath, scssFormattedData);
 
-        // Create extra js files in the component dir
+        // Create extra ts files in the component dir
         for (let i = 0; i < files.length; i++) {
             await fs.appendFile(`${cmpDir}/${files[i]}.ts`, '');
         }
@@ -329,18 +357,16 @@ const createStoryFiles = async ({ name, level, props }) => {
     }
 };
 
-const addTsInRollup = async ({ name }) => {
+const createTestFiles = async ({ name, level, portal }) => {
     try {
-        const rollupPath = path.join(__dirname, '..', 'configs', 'rollup.config.js');
-        const data = await fs.readFile(rollupPath, 'utf-8');
-        const regex = /const TSComponentsList = \[(.*?)\]/s;
-        const match = data.match(regex)?.[0];
-        let dataForReplace = '';
-        if (match) {
-            dataForReplace = match.replace(']', `, '${name}']`);
-        }
-        const newData = data.replace(match, dataForReplace);
-        await fs.writeFile(rollupPath, prettier.format(newData, prettierConfig));
+        const srcCode = generateCmpTestTemplate({
+            name,
+            portal
+        });
+
+        const testDir = path.join(__dirname, ...pathToLib, `${level}`, `${name}`, `${name}.test.tsx`);
+        const formattedData = prettier.format(srcCode, { ...prettierConfig, parser: 'typescript' });
+        await fs.writeFile(testDir, formattedData);
     } catch (error) {
         return {
             hasError: true,
@@ -349,36 +375,18 @@ const addTsInRollup = async ({ name }) => {
     }
 };
 
-const createTestFiles = async ({ name, level, portal }) => {
-    const beforeEach = {
-        yes: `beforeEach(() => (setup = mount(<${name} />, { wrappingComponent: GeneUIProvider })));`,
-        no: `beforeEach(() => (setup = mount(<${name} />)));`
-    };
-
+const addTsInRollup = async ({ name }) => {
     try {
-        const testDir = path.join(__dirname, ...pathToLib, `${level}`, `${name}`, `${name}.test.tsx`);
-        const IComponentProps = `I${name}Props`;
-        const testFileData = `
-            import React from 'react';
-            import { ReactWrapper, mount } from 'enzyme';
-            
-            // Components
-            import ${name}, { ${IComponentProps} } from './index';
-            ${portal === 'yes' ? `import GeneUIProvider from '../../providers/GeneUIProvider';` : ''}
-            
-            describe('${name} ', () => {
-                let setup: ReactWrapper<${IComponentProps}>;
-                ${beforeEach[portal]}
-
-                it('renders without crashing', () => {
-                    expect(setup.exists()).toBeTruthy();
-                });
-
-                //your tests here
-            });
-        `;
-        const formattedData = prettier.format(testFileData, { ...prettierConfig, parser: 'typescript' });
-        await fs.writeFile(testDir, formattedData);
+        const rollupPath = path.join(__dirname, '..', 'configs', 'rollup.config.js');
+        const data = await fs.readFile(rollupPath, 'utf-8');
+        const regex = /const TSComponentsList = \[(.*?)\]/s;
+        const [match] = data.match(regex);
+        let dataForReplace = '';
+        if (match) {
+            dataForReplace = match.replace(']', `, '${name}']`);
+        }
+        const newData = data.replace(match, dataForReplace);
+        await fs.writeFile(rollupPath, prettier.format(newData, prettierConfig));
     } catch (error) {
         return {
             hasError: true,
@@ -416,7 +424,6 @@ const addGlobalExportToIndexTs = async ({ level, name }) => {
             }
         }
     } catch (error) {
-        console.log(error);
         return {
             hasError: true,
             error
