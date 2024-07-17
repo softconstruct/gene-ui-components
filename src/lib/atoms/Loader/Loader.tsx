3645da4bf750ad264cc5a3cda30126f69945e12f
Sos <sos.petrosyan@betconstruct.com>
2024-07-17 14:15:02 +0400

    feat(Loader): component UI

--- src/lib/atoms/Loader/Loader.tsx
+++ src/lib/atoms/Loader/Loader.tsx
@@ -11,7 +11,14 @@ interface ILoaderProps {
 }
 
 const Loader: FC<ILoaderProps> = ({ isBusy }) => {
-    return <div className="loader">Loader</div>;
+    return (
+        <span className="loader loader_direction_horizontal loader_size_2xlarge loader_color_brand">
+            <svg className="loader__spinner" viewBox="0 0 50 50">
+                <circle className="loader__spinner_path" cx="25" cy="25" r="20" fill="none" strokeWidth="5"></circle>
+            </svg>
+            <span className="loader__text loader__text_color_brand">Loading Info</span>
+        </span>
+    );
 };
 
 export { ILoaderProps, Loader as default };
