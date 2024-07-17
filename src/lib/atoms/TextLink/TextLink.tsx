057af70e220d456426d192251614f3a20f78c28b
karapetyan.henrik <henrik.karapetyan@betconstruct.com>
2024-07-17 14:14:25 +0400

    feat(TextLink): component UI

--- src/lib/atoms/TextLink/TextLink.tsx
+++ src/lib/atoms/TextLink/TextLink.tsx
@@ -14,7 +14,37 @@ interface ITextLinkProps {
  * A link is styled text that navigates users to another location, either within the current experience or to a different app or website.
  */
 const TextLink: FC<ITextLinkProps> = ({ type }) => {
-    return <div className="textLink">TextLink</div>;
+    return (
+        <div className="textLinkTestHolder">
+            <a className="textLink textLink_color_primary" href="#">
+                <span className="textLink__text">text link</span>
+            </a>
+            <a className="textLink textLink_underline textLink_color_primary" href="#">
+                <span className="textLink__text">text link</span>
+            </a>
+            <a className="textLink textLink_disabled textLink_color_primary" href="#">
+                <span className="textLink__text">text link</span>
+            </a>
+            <a className="textLink textLink_color_secondary" href="#">
+                <span className="textLink__text">text link</span>
+            </a>
+            <a className="textLink textLink_underline textLink_color_secondary" href="#">
+                <span className="textLink__text">text link</span>
+            </a>
+            <a className="textLink textLink_disabled textLink_color_secondary" href="#">
+                <span className="textLink__text">text link</span>
+            </a>
+            <a className="textLink textLink_color_inverse" href="#">
+                <span className="textLink__text">text link</span>
+            </a>
+            <a className="textLink textLink_underline textLink_color_inverse" href="#">
+                <span className="textLink__text">text link</span>
+            </a>
+            <a className="textLink textLink_disabled textLink_color_inverse" href="#">
+                <span className="textLink__text">text link</span>
+            </a>
+        </div>
+    );
 };
 
 export { ITextLinkProps, TextLink as default };
