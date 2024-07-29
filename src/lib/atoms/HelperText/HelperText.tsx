7e366d57540c61a7f3f94498784a33ec66fb8316
Sos <sos.petrosyan@betconstruct.com>
2024-07-29 15:47:59 +0400

    feat(HelperText): component UI

--- src/lib/atoms/HelperText/HelperText.tsx
+++ src/lib/atoms/HelperText/HelperText.tsx
@@ -2,6 +2,7 @@ import React, { FC } from 'react';
 
 // Styles
 import './HelperText.scss';
+import { Icon } from '../../../index';
 
 interface IHelperTextProps {
     /**
@@ -14,7 +15,29 @@ interface IHelperTextProps {
  * The Helper Text provides users an additional information or guidance related to a specific input field in a form. This text helps users understand the expected format, requirements, or purpose of the input, thereby improving form completion accuracy and user confidence.
  */
 const HelperText: FC<IHelperTextProps> = ({ size }) => {
-    return <div className="helperText">HelperText</div>;
+    return (
+        <div className="helperTextTestHolder">
+            {/* Please add following classNames // helperText_size_medium /or/ helperText_size_small */}
+            <div className="helperText helperText_state_rest helperText_size_medium">
+                <div className="helperText__icon">
+                    <Icon type={'bc-icon-info-48'} disabled={false} isFilled={true} />
+                </div>
+                <p className="helperText__text">Helper Text</p>
+            </div>
+            <div className="helperText helperText_state_danger helperText_size_medium">
+                <div className="helperText__icon">
+                    <Icon type={'bc-icon-info-48'} disabled={false} isFilled={true} />
+                </div>
+                <p className="helperText__text">Helper Text</p>
+            </div>
+            <div className="helperText helperText_state_disabled helperText_size_medium">
+                <div className="helperText__icon">
+                    <Icon type={'bc-icon-info-48'} disabled={false} isFilled={true} />
+                </div>
+                <p className="helperText__text">Helper Text</p>
+            </div>
+        </div>
+    );
 };
 
 export { IHelperTextProps, HelperText as default };
