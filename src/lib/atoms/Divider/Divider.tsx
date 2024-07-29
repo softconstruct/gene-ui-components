be3ae4cc9d914e4d711feac7f422833dc10e4dd8
Sos <sos.petrosyan@betconstruct.com>
2024-07-29 13:28:06 +0400

    feat(Divider): component UI

--- src/lib/atoms/Divider/Divider.tsx
+++ src/lib/atoms/Divider/Divider.tsx
@@ -2,6 +2,7 @@ import React, { FC } from 'react';
 
 // Styles
 import './Divider.scss';
+import { Icon } from '../../../index';
 
 interface IDividerProps {
     /**
@@ -14,7 +15,43 @@ interface IDividerProps {
  * A divider separates sections of content to establish visual rhythm and hierarchy. Combine dividers with appropriate spacing and text hierarchy to effectively organize content within your layout.
  */
 const Divider: FC<IDividerProps> = ({ type }) => {
-    return <div className="divider">Divider</div>;
+    return (
+        <div className="dividerTestHolder">
+            {/* This component created partially because of missing many cases in design */}
+            <div className="divider divider_color_default">
+                <div className="divider__label">
+                    <label htmlFor="#id" className="divider__labelText">
+                        Label
+                    </label>
+                    <Icon className="divider__labelIcon" type={'bc-icon-info'} disabled={false} isFilled={true} />
+                </div>
+            </div>
+            <div className="divider divider_color_strong">
+                <div className="divider__label">
+                    <label htmlFor="#id" className="divider__labelText">
+                        Label
+                    </label>
+                    <Icon className="divider__labelIcon" type={'bc-icon-info'} disabled={false} isFilled={true} />
+                </div>
+            </div>
+            <div className="divider divider_color_brand">
+                <div className="divider__label">
+                    <label htmlFor="#id" className="divider__labelText">
+                        Label
+                    </label>
+                    <Icon className="divider__labelIcon" type={'bc-icon-info'} disabled={false} isFilled={true} />
+                </div>
+            </div>
+            <div className="divider divider_color_inverse">
+                <div className="divider__label">
+                    <label htmlFor="#id" className="divider__labelText">
+                        Label
+                    </label>
+                    <Icon className="divider__labelIcon" type={'bc-icon-info'} disabled={false} isFilled={true} />
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export { IDividerProps, Divider as default };
