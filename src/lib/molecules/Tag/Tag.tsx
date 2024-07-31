2a991c35a1b31b0b00768aa2aad5eb467bb80e0c
Sos <sos.petrosyan@betconstruct.com>
2024-07-31 09:17:59 +0400

    feat(Tag): component UI

--- src/lib/molecules/Tag/Tag.tsx
+++ src/lib/molecules/Tag/Tag.tsx
@@ -14,7 +14,29 @@ interface ITagProps {
  * A tag component is a small, interactive element used to label, categorize, and organize content within an interface. It can be used to highlight keywords, topics, or attributes related to an item. Tags enhance user navigation and search functionality by providing a quick way to filter and identify relevant information.
  */
 const Tag: FC<ITagProps> = ({ size }) => {
-    return <div className="tag">Tag</div>;
+    return (
+        <div className="tagTestHolder">
+            <div className="tag tag_size_medium tag_state_rest tag_icon_false">
+                <p className="tag__text">Tag</p>
+                <button className="tag__button"></button>
+            </div>
+
+            <div className="tag tag_size_medium tag_state_error tag_icon_false">
+                <p className="tag__text">Tag</p>
+                <button className="tag__button"></button>
+            </div>
+
+            <div className="tag tag_size_medium tag_state_warning tag_icon_false">
+                <p className="tag__text">Tag</p>
+                <button className="tag__button"></button>
+            </div>
+
+            <div className="tag tag_size_medium tag_state_disabled tag_icon_false">
+                <p className="tag__text">Tag</p>
+                <button className="tag__button"></button>
+            </div>
+        </div>
+    );
 };
 
 export { ITagProps, Tag as default };
