bf75f4356086852a2a7d985315563399959ffd0c
Galust <galust.gabrielyan@softconstruct.com>
2024-09-06 17:41:29 +0400

    fix(Header): uI

--- src/lib/organisms/Header/Header.tsx
+++ src/lib/organisms/Header/Header.tsx
@@ -14,7 +14,7 @@ interface IHeaderProps {
  * Global Header component is a persistent navigation element that appears at the top of an application or website. It serves as a central hub for accessing key features and tools, ensuring consistent and intuitive navigation across all pages.
  */
 const Header: FC<IHeaderProps> = ({ type }) => {
-    return <div className="header">Header</div>;
+    return <header className="header">Header component</header>;
 };
 
 export { IHeaderProps, Header as default };
