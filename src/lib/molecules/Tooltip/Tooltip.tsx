e32c36a8c840f0bef8ddbd0aac59c4ee8e6bbb20
Sos <sos.petrosyan@betconstruct.com>
2024-10-10 12:59:51 +0400

    fix(Tooltip): changed arrow to svg

--- src/lib/molecules/Tooltip/Tooltip.tsx
+++ src/lib/molecules/Tooltip/Tooltip.tsx
@@ -279,7 +279,17 @@ const Tooltip: FC<ITooltipProps> = ({
                                 top: middlewareArrowData?.y || undefined,
                                 [staticSide!]: arrowRef.current ? `${-arrowRef?.current?.offsetWidth + 5}px` : 0
                             }}
-                        />
+                        >
+                            <svg
+                                width="12"
+                                height="4"
+                                viewBox="0 0 12 4"
+                                fill="none"
+                                xmlns="http://www.w3.org/2000/svg"
+                            >
+                                <path className="tooltip__arrowPath" d="M6 4L0 0L12 0L6 4Z" fill="white" />
+                            </svg>
+                        </div>
 
                         <div className="tooltip__textWrapper">
                             <p className="tooltip__text">{text}</p>
