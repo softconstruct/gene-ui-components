88addbddc8cdd4861bf28876b648670763dcc0f7
Sos <sos.petrosyan@betconstruct.com>
2024-10-16 11:37:40 +0400

    feat(Banner): ui development

--- src/lib/molecules/Banner/Banner.tsx
+++ src/lib/molecules/Banner/Banner.tsx
@@ -11,7 +11,16 @@ interface IBannerProps {
  * Banner component is a prominent, horizontally-oriented message box designed to capture the user's attention and convey important information across the top of a page. It is used for announcements, alerts, promotions, or updates that need to be immediately visible to users.
  */
 const Banner: FC<IBannerProps> = (props) => {
-    return <div className="banner">Banner</div>;
+    return (
+        <div className="banner banner_state_informative">
+            {/* there are following states banner_state_informative // banner_state_warning // banner_state_error */}
+            <div className="banner__content">
+                <div className="banner__icon">i</div>
+                <p className="banner__text">Description text goes here.</p>
+            </div>
+            <div className="banner__actions"></div>
+        </div>
+    );
 };
 
 export { IBannerProps, Banner as default };
