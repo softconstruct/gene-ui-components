771a534fd3a40791db354dd4336ce005df5846be
Sos <sos.petrosyan@betconstruct.com>
2024-11-04 15:36:35 +0400

    feat(ProgressBar): component UI

--- src/components/molecules/ProgressBar/ProgressBar.tsx
+++ src/components/molecules/ProgressBar/ProgressBar.tsx
@@ -2,6 +2,8 @@ import React, { FC } from "react";
 import classNames from "classnames";
 // Styles
 import "./ProgressBar.scss";
+import HelperText from "../../atoms/HelperText";
+import Label from "../../atoms/Label";
 
 interface IProgressBarProps {
     /**
@@ -16,7 +18,28 @@ interface IProgressBarProps {
  * A progress bar offers visual feedback on the status and duration of a process, such as a download, file transfer, or installation, helping users understand how much longer they need to wait.
  */
 const ProgressBar: FC<IProgressBarProps> = ({ className }) => {
-    return <div className={classNames("progressBar", className)}>ProgressBar</div>;
+    // Sizes / large / medium / small
+    // Types / determinate / indeterminate
+    // Colors / default / success / error
+
+    return (
+        <div
+            className={classNames(
+                "progressBar progressBar_type_indeterminate progressBar_size_large progressBar_color_default",
+                className
+            )}
+        >
+            <Label required labelText="Label" />
+            <div className="progressBar__track">
+                <div className="progressBar__fill" style={{ width: "50%" }} />
+                <div className="progressBar__loadingBar" />
+            </div>
+            <div className="progressBar__info">
+                <HelperText text="Helper Text" />
+                <p className="progressBar__status">Uploading 10%</p>
+            </div>
+        </div>
+    );
 };
 
 export { IProgressBarProps, ProgressBar as default };
