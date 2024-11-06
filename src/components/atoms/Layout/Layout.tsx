4d535c061738e8565dd1ead310cb48e9c643fb87
Sos <sos.petrosyan@betconstruct.com>
2024-11-06 17:14:49 +0400

    feat(Layout): component UI

--- src/components/atoms/Layout/Layout.tsx
+++ src/components/atoms/Layout/Layout.tsx
@@ -16,7 +16,16 @@ interface ILayoutProps {
  * Layout is the foundation of systematic visual design. Unlike traditional graphic design, the layout space of a UI interface should be approached from a dynamic and systematic perspective.
  */
 const Layout: FC<ILayoutProps> = ({ className }) => {
-    return <div className={classNames("layout", className)}>Layout</div>;
+    return (
+        <div className={classNames("layout layout_doubleSidebars", className)}>
+            <header className="header" />
+            <main className="content" />
+            <aside className="sidebar sidebar_left sidebar_left_collapsed sidebar_hasAside">
+                <div className="sidebar__aside" />
+            </aside>
+            <aside className="sidebar sidebar_right" />
+        </div>
+    );
 };
 
 export { ILayoutProps, Layout as default };
