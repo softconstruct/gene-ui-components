c3d54f61fd40950a71a8fa2ee79d2402a4526149
abrahamyan.karen <abrahamyan.karen@softconstruct.com>
2024-11-06 17:41:19 +0400

    feat(Switch): component UI

--- src/components/molecules/Switch/Switch.tsx
+++ src/components/molecules/Switch/Switch.tsx
@@ -2,6 +2,8 @@ import React, { FC } from "react";
 import classNames from "classnames";
 // Styles
 import "./Switch.scss";
+import { CheckMark } from "@geneui/icons";
+import HelperText from "../../atoms/HelperText";
 
 interface ISwitchProps {
     /**
@@ -16,7 +18,19 @@ interface ISwitchProps {
  * A switch component allows users to toggle between two states, typically "on" and "off". It is commonly used in settings and preferences to enable or disable features or functionalities.
  */
 const Switch: FC<ISwitchProps> = ({ className }) => {
-    return <div className={classNames("switch", className)}>Switch</div>;
+    return (
+        // todo: switch in the next classNames for labelText alignment: "switch_labelAfter", "switch_labelBefore", "switch_labelTop"
+        <div className={classNames("switch switch_labelAfter", className)}>
+            <label className="switch__label">
+                <input type="checkbox" className="switch__input" disabled={false} readOnly={false} />
+                <span className="switch__slider">
+                    <CheckMark size={16} className="switch__icon" />
+                </span>
+                <span className="switch__labelText">Label</span>
+            </label>
+            <HelperText text="Helper Text" className="switch__helperText" />
+        </div>
+    );
 };
 
 export { ISwitchProps, Switch as default };
