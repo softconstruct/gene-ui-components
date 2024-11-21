cf679801a4da753432685b58d150c673173796a8
arshakyan.narek <arshakyan.narek@softconstruct.com>
2024-11-21 17:54:19 +0400

    feat(Grid): add functionality

--- src/components/atoms/Grid/Grid.tsx
+++ src/components/atoms/Grid/Grid.tsx
@@ -1,23 +1,23 @@
-import React, { FC, PropsWithChildren } from 'react';
+import React, { FC, PropsWithChildren } from "react";
 
 // Styles
-import './Grid.scss';
+import "./Grid.scss";
 
 export interface IRow extends PropsWithChildren {
     flexible?: boolean;
 }
 
 export interface ICol extends PropsWithChildren {
-    size: number | 'fixed';
+    size: number | "fixed";
     offset?: 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12;
 }
 
 const Row: FC<IRow> = ({ children, flexible }) => {
-    return <div className={`row ${flexible ? 'flexible' : ''}`}>{children}</div>;
+    return <div className={`row ${flexible ? "flexible" : ""}`}>{children}</div>;
 };
 
 const Col: FC<ICol> = ({ children, size, offset }) => {
-    return <div className={`col-${size} ${offset ? `col-offset-${offset}` : ''}`}>{children}</div>;
+    return <div className={`col-${size} ${offset ? `col-offset-${offset}` : ""}`}>{children}</div>;
 };
 
 const GridContainer: FC<PropsWithChildren> = ({ children }) => {
