0f912593f92de67e6c6212b382082bc230645ae7
Sos <sos.petrosyan@betconstruct.com>
2024-12-09 13:22:19 +0400

    feat(Badge): component UI

--- src/components/atoms/Badge/Badge.tsx
+++ src/components/atoms/Badge/Badge.tsx
@@ -2,6 +2,7 @@ import React, { FC } from "react";
 import classNames from "classnames";
 // Styles
 import "./Badge.scss";
+import { Button } from "../../../index";
 
 interface IBadgeProps {
     /**
@@ -16,7 +17,17 @@ interface IBadgeProps {
  * Numeric Badge component is a small, circular indicator that displays numerical information, often used to highlight counts or statuses. It is typically positioned adjacent to icons or labels, providing users with a quick visual cue about the number of notifications, messages, or items requiring attention.
  */
 const Badge: FC<IBadgeProps> = ({ className }) => {
-    return <div className={classNames("badge", className)}>Badge</div>;
+    return (
+        <div className={classNames("badge", className)}>
+            <div className="badge__content badge__content_position_absolute badge__content_size_small badge__content_color_brand badge__content_bordered">
+                {/* size - // badge_size_small // badge_size_smallNudge // badge_size_xSmall // badge_size_3xSmall */}
+                {/* color - // badge_color_brand // badge_color_neutral //badge_color_red // badge_color_inverse */}
+                {/* border - // badge_bordered */}
+                <p className="badge__num">1</p>
+            </div>
+            <Button appearance="danger" text="Button" />
+        </div>
+    );
 };
 
 export { IBadgeProps, Badge as default };
