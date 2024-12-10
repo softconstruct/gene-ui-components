99c0004a80b7b8190a5a9b2237e64891e264d4b1
Arthur Grigoryan <arthur.grigoryan@softconstruct.com>
2024-12-10 16:03:02 +0400

    feat(Tag): new component

--- src/components/molecules/Tag/Tag.test.tsx
+++ src/components/molecules/Tag/Tag.test.tsx
@@ -27,4 +27,30 @@ describe("Tag ", () => {
     it("renders content text correctly", () => {
         expect(setup.find(".tag__text").contains(tagText)).toBeTruthy();
     });
+
+    it("renders passed state correctly", () => {
+        expect(setup.find(".tag_state_rest").exists()).toBeTruthy();
+        const wrapper = setup.setProps({ state: "error" });
+        expect(wrapper.find(".tag_state_error").exists()).toBeTruthy();
+    });
+
+    it("renders passed size correctly", () => {
+        expect(setup.find(".tag_size_medium").exists()).toBeTruthy();
+        const wrapper = setup.setProps({ size: "small" });
+        expect(wrapper.find(".tag_size_small").exists()).toBeTruthy();
+    });
+
+    it("renders with icon", () => {
+        expect(setup.find(".tag__icon").exists()).toBeTruthy();
+    });
+
+    it("renders without icon", () => {
+        const wrapper = setup.setProps({ withIcon: false });
+        expect(wrapper.find(".tag__icon").exists()).toBeFalsy();
+    });
+
+    it("should call onClose when close button is pressed", () => {
+        setup.find(".tag__button .button_icon_only").simulate("click");
+        expect(mockFn).toHaveBeenCalled();
+    });
 });
