893a04c44ba00abbb98c2d0e70ee49cfb75a063d
Sos <sos.petrosyan@betconstruct.com>
2024-12-10 09:46:17 +0400

    feat(Segmented Control): component UI

--- src/components/atoms/SegmentedControl/SegmentedControl.tsx
+++ src/components/atoms/SegmentedControl/SegmentedControl.tsx
@@ -2,6 +2,9 @@ import React, { FC } from "react";
 import classNames from "classnames";
 // Styles
 import "./SegmentedControl.scss";
+// import {ArrowBigLeft} from "lucide-react";
+import { TagOutline } from "@geneui/icons";
+import { HelperText, Label } from "../../../index";
 
 interface ISegmentedControlProps {
     /**
@@ -13,7 +16,42 @@ interface ISegmentedControlProps {
 }
 
 const SegmentedControl: FC<ISegmentedControlProps> = ({ className }) => {
-    return <div className={classNames("segmentedControl", className)}>SegmentedControl</div>;
+    return (
+        <div className={classNames("segmentedControl", className)}>
+            <Label labelText="Label" required />
+            <div className="segmentedControl__wrapper">
+                <button
+                    type="button"
+                    className="segmentedControl__block segmentedControl__block_size_large segmentedControl__block_icon_before segmentedControl__block_selected"
+                >
+                    <TagOutline />
+                    <span className="segmentedControl__text">Button</span>
+                </button>
+                <button
+                    type="button"
+                    className="segmentedControl__block segmentedControl__block_size_large segmentedControl__block_icon_before"
+                >
+                    <span className="segmentedControl__text">Button</span>
+                </button>
+                <button type="button" className="segmentedControl__block segmentedControl__block_size_large">
+                    <TagOutline />
+                    <span className="segmentedControl__text">Button</span>
+                </button>
+                <button type="button" className="segmentedControl__block segmentedControl__block_size_large">
+                    <span className="segmentedControl__text">Button</span>
+                </button>
+                <button
+                    type="button"
+                    className="segmentedControl__block segmentedControl__block_size_large segmentedControl__block_icon_after segmentedControl__block_selected"
+                    disabled
+                >
+                    <TagOutline />
+                    <span className="segmentedControl__text">Button</span>
+                </button>
+            </div>
+            <HelperText text="Helper text" />
+        </div>
+    );
 };
 
 export { ISegmentedControlProps, SegmentedControl as default };
