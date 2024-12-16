579121b1c782e16fad07d40ae46d795d38cfa87f
Arthur Grigoryan <arthur.grigoryan@softconstruct.com>
2024-12-16 11:55:14 +0400

    feat(Tag): new component

--- src/components/molecules/Tag/Tag.tsx
+++ src/components/molecules/Tag/Tag.tsx
@@ -60,7 +60,7 @@ const Tag: FC<ITagProps> = ({
     withIcon = true,
     onClose
 }) => {
-    const Icon = icons[type];
+    const Icon = isDisabled ? icons.rest : icons[type];
     return (
         <div
             className={classNames(
