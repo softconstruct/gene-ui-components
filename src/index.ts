9bedd8ede791baea9034d49152c8f033a207383e
GaroGabrielyan <150226553+GaroGabrielyan@users.noreply.github.com>
2025-02-12 19:10:10 +0400

    feat(Steps): new component (#248)

--- src/index.ts
+++ src/index.ts
@@ -12,8 +12,9 @@ export { default as Scrollbar, IScrollbarProps } from "./components/atoms/Scroll
 export { Grid, Col, Row, IColProps, IRowProps } from "./components/atoms/Grid";
 
 // Molecules
-export { default as Tooltip, ITooltipProps } from "./components/molecules/Tooltip";
-export { default as ProgressBar, IProgressBarProps } from "./components/molecules/ProgressBar";
+export { default as Tooltip } from "./components/molecules/Tooltip";
+export { default as ProgressBar } from "./components/molecules/ProgressBar";
+export { Steps, Step, IStepProps, IStepsProps } from "./components/molecules/Steps";
 export { default as Tag, ITagProps } from "./components/molecules/Tag";
 
 // Organisms
