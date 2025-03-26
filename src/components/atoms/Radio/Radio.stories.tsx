9549808a6979a6d67fe33d9f9ce8b7cc61d62da0
Hamik Hambardzumyan <hamik.hambardzumyan@softconstruct.com>
2025-03-26 18:34:38 +0400

    docs(Radio): polish doc of the component

--- src/components/atoms/Radio/Radio.stories.tsx
+++ src/components/atoms/Radio/Radio.stories.tsx
@@ -10,6 +10,8 @@ const meta: Meta<IRadioProps> = {
     title: "Atoms/Radio",
     component: Radio,
     argTypes: {
+        name: args({ control: "text", ...propCategory.others }),
+        value: args({ control: "text", ...propCategory.others }),
         label: args({ control: "text", ...propCategory.content }),
         infoText: args({ control: "text", ...propCategory.content }),
         disabled: args({ control: "boolean", ...propCategory.states }),
@@ -21,7 +23,6 @@ const meta: Meta<IRadioProps> = {
         type: args({ control: "select", ...propCategory.appearance }),
         direction: args({ control: "select", ...propCategory.appearance }),
         autoFocus: args({ control: "boolean", ...propCategory.functionality }),
-        name: args({ control: "text", ...propCategory.others }),
         onChange: args({ control: "false", ...propCategory.action }),
         onBlur: args({ control: "false", ...propCategory.action }),
         onFocus: args({ control: "false", ...propCategory.action }),
