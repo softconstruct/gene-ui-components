3e8dc8437dba1bdf5e3d17d9dd3df1becea62989
naira.misakyan <naira.misakyan@betconstruct.am>
2025-04-07 13:07:58 +0400

    feat(TagGroup): add Tests for TagGroup component

--- src/components/molecules/TagGroup/TagGroup.test.tsx
+++ src/components/molecules/TagGroup/TagGroup.test.tsx
@@ -12,7 +12,6 @@ describe("TagGroup ", () => {
         setup = mount(
             <TagGroup>
                 <Tag text="Tab" />
-                <Tag text="Tab" />
             </TagGroup>
         );
     });
@@ -21,24 +20,14 @@ describe("TagGroup ", () => {
         expect(setup.exists()).toBeTruthy();
     });
 
-    // it("renders className prop correctly", () => {
-    //     const className = "test-class";
-    //     const wrapper = setup.setProps({ className });
+    it("renders className prop correctly", () => {
+        const className = "test-class";
+        const wrapper = setup.setProps({ className });
 
-    //     expect(wrapper.hasClass(className)).toBeTruthy();
-    // });
+        expect(wrapper.hasClass(className)).toBeTruthy();
+    });
 
-    // it("renders multiple tags correctly", () => {
-    //     expect(setup.find(Tag)).toHaveLength(2);
-    // });
-
-    // it("removes a tag when close button is clicked", () => {
-    //     expect(setup.find(Tag)).toHaveLength(2);
-
-    //     // Simulate closing the first tag
-    //     setup.find(Tag).first().props().onClose?.();
-    //     setup.mount();
-
-    //     expect(setup.find(Tag)).toHaveLength(1);
-    // });
+    it("renders children prop correctly", () => {
+        expect(setup.find(Tag)).toBeTruthy();
+    });
 });
