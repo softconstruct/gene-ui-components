fb83984fd87069a529529e3e864100210486b893
Hamik Hambardzumyan <hamik.hambardzumyan@softconstruct.com>
2025-04-17 13:21:23 +0400

    feat(Spreadsheet): add logic tests and story for the component

--- src/components/atoms/Spreadsheet/Spreadsheet.stories.tsx
+++ src/components/atoms/Spreadsheet/Spreadsheet.stories.tsx
@@ -1,8 +1,11 @@
+import React, { useState } from "react";
 import { Meta, StoryObj } from "@storybook/react";
 
 // Helpers
 import { args, propCategory } from "../../../../stories/assets/storybook.globals";
 // Components
+import Button from "../Button";
+import Text from "../Text";
 import Spreadsheet, { ISpreadsheetProps } from "./index";
 
 const meta: Meta<ISpreadsheetProps> = {
@@ -10,10 +13,13 @@ const meta: Meta<ISpreadsheetProps> = {
     component: Spreadsheet,
     argTypes: {
         className: args({ control: "false", ...propCategory.appearance }),
-        inset: args({ control: "false", ...propCategory.appearance }),
-        children: args({ control: "false", ...propCategory.content })
+        open: args({ control: "boolean", ...propCategory.functionality }),
+        inset: args({ control: "boolean", ...propCategory.appearance }),
+        children: args({ control: "false", ...propCategory.content }),
+        onClose: args({ control: "false", ...propCategory.content })
     },
     args: {
+        open: false,
         inset: false
     }
 };
@@ -22,4 +28,98 @@ export default meta;
 
 type Story = StoryObj<ISpreadsheetProps>;
 
-export const Default: Story = {};
+export const Default: Story = {
+    render: (props) => {
+        // eslint-disable-next-line react-hooks/rules-of-hooks
+        const [isOpen, setIsOpen] = useState(props.open);
+
+        return (
+            <>
+                <Button onClick={() => setIsOpen(true)}>Open Spreadsheet</Button>
+
+                <Text as="p">
+                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
+                    industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type
+                    scrambled it to make a type specimen book. It has survived not only five centuries, but also the
+                    leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s
+                    with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop
+                    publishing software like Aldus PageMaker including versions of Lorem Ipsum. Lorem Ipsum is simply
+                    dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard
+                    dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to
+                    make a type specimen book. It has survived not only five centuries, but also the leap into
+                    electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the
+                    release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop
+                    publishing software like Aldus PageMaker including versions of Lorem Ipsum. Lorem Ipsum is simply
+                    dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard
+                    dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to
+                    make a type specimen book. It has survived not only five centuries, but also the leap into
+                    electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the
+                    release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop
+                    publishing software like Aldus PageMaker including versions of Lorem Ipsum. Lorem Ipsum is simply
+                    dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard
+                    dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to
+                    make a type specimen book. It has survived not only five centuries, but also the leap into
+                    electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the
+                    release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop
+                    publishing software like Aldus PageMaker including versions of Lorem Ipsum. Lorem Ipsum is simply
+                    dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard
+                    dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to
+                    make a type specimen book. It has survived not only five centuries, but also the leap into
+                    electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the
+                    release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop
+                    publishing software like Aldus PageMaker including versions of Lorem Ipsum. Lorem Ipsum is simply
+                    dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard
+                    dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to
+                    make a type specimen book. It has survived not only five centuries, but also the leap into
+                    electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the
+                    release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop
+                    publishing software like Aldus PageMaker including versions of Lorem Ipsum. Lorem Ipsum is simply
+                    dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard
+                    dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to
+                    make a type specimen book. It has survived not only five centuries, but also the leap into
+                    electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the
+                    release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop
+                    publishing software like Aldus PageMaker including versions of Lorem Ipsum. Lorem Ipsum is simply
+                    dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard
+                    dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to
+                    make a type specimen book. It has survived not only five centuries, but also the leap into
+                    electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the
+                    release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop
+                    publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+                </Text>
+
+                <Button onClick={() => setIsOpen(true)}>Open Spreadsheet</Button>
+
+                <Spreadsheet {...props} open={isOpen} onClose={() => setIsOpen(false)}>
+                    <Text as="p">
+                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been
+                        the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of
+                        type and scrambled it to make a type specimen book. It has survived not only five centuries, but
+                        also the leap into electronic typesetting, remaining essentially unchanged. It was popularised
+                        in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more
+                        recently with desktop publishing software like Aldus PageMaker including versions of Lorem
+                        Ipsum. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum
+                        has been the industry standard dummy text ever since the 1500s, when an unknown printer took a
+                        galley of type and scrambled it to make a type specimen book. It has survived not only five
+                        centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It
+                        was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum
+                        passages, and more recently with desktop publishing software like Aldus PageMaker including
+                        versions of Lorem Ipsum. Lorem Ipsum is simply dummy text of the printing and typesetting
+                        industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an
+                        unknown printer took a galley of type and scrambled it to make a type specimen book. It has
+                        survived not only five centuries, but also the leap into electronic typesetting, remaining
+                        essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets
+                        containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus
+                        PageMaker including versions of Lorem Ipsum. Lorem Ipsum is simply dummy text of the printing
+                        and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the
+                        1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen
+                        book. It has survived not only five centuries, but also the leap into electronic typesetting,
+                        remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset
+                        sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like
+                        Aldus PageMaker including versions of Lorem Ipsum.
+                    </Text>
+                </Spreadsheet>
+            </>
+        );
+    }
+};
--- src/components/atoms/Spreadsheet/Spreadsheet.tsx
+++ src/components/atoms/Spreadsheet/Spreadsheet.tsx
@@ -1,7 +1,14 @@
-import React, { FC } from "react";
+import React, { FC, useContext, useEffect } from "react";
+import { createPortal } from "react-dom";
 import classNames from "classnames";
 
+// Components
 import Scrollbar from "@components/atoms/Scrollbar";
+import { GeneUIDesignSystemContext } from "@components/providers/GeneUIProvider";
+
+// Hooks
+import useClickOutside from "@hooks/useClickOutside";
+import useScrollLock from "@hooks/useScrollLock";
 
 // Styles
 import "./Spreadsheet.scss";
@@ -13,11 +20,23 @@ interface ISpreadsheetProps {
      */
     className?: string;
     /**
-     * inset description
+     * Controls the visibility of the Spreadsheet component.
+     * When `true`, the component is displayed as an overlay. When `false`, it is hidden.
+     */
+    open: boolean;
+    /**
+     * A callback function triggered when a close interaction is detected (e.g., click outside).
+     * Use this to handle cleanup or update the state controlling open.
+     */
+    onClose?: () => void;
+    /**
+     * Controls whether padding/inset styles are applied to the content area inside the Spreadsheet.
+     * Set to `false` to remove internal spacing for edge-to-edge content.
      */
     inset?: boolean;
     /**
-     * children description
+     * The content to render inside the Spreadsheet.
+     * Typically includes form elements, info panels, or custom UI blocks.
      */
     children?: React.ReactElement;
 }
@@ -25,18 +44,35 @@ interface ISpreadsheetProps {
 /**
  * The Spreadsheet component is a mobile-specific layout container designed to fully cover the Popover in mobile view. It acts as a structured content shell for displaying or editing contextual information triggered by a Popover — giving users a focused, full-screen experience on smaller screens.
  */
-const Spreadsheet: FC<ISpreadsheetProps> = ({ inset, children, className }) => {
-    // eslint-disable-next-line no-console
-    console.log("🚀 ~ children:", children);
-    // eslint-disable-next-line no-console
-    console.log("🚀 ~ inset:", inset);
+const Spreadsheet: FC<ISpreadsheetProps> = ({ open, inset = true, onClose = () => {}, children, className }) => {
+    const bodyRef = useClickOutside(onClose);
+    const { lock, unlock } = useScrollLock(document.body);
+
+    const { geneUIProviderRef } = useContext(GeneUIDesignSystemContext);
+
+    useEffect(() => {
+        if (open) {
+            lock();
+        } else {
+            unlock();
+        }
+    }, [open]);
 
     return (
-        <div className={classNames("spreadsheet", className)}>
-            <div className={`spreadsheet__body ${inset ? `spreadsheet__body_inset` : ""}`}>
-                <Scrollbar>{children}</Scrollbar>
-            </div>
-        </div>
+        <>
+            {open && geneUIProviderRef.current
+                ? createPortal(
+                      <div className={classNames("spreadsheet", className)}>
+                          <div ref={bodyRef} className="spreadsheet__body">
+                              <Scrollbar>
+                                  <div className={classNames({ spreadsheet__body_inset: inset })}>{children}</div>
+                              </Scrollbar>
+                          </div>
+                      </div>,
+                      geneUIProviderRef.current
+                  )
+                : null}
+        </>
     );
 };
 
