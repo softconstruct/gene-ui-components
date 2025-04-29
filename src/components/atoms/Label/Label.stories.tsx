e95e7e7808989ed259506df476c9cfc9064f1d25
naira.misakyan <naira.misakyan@betconstruct.am>
2025-04-29 11:04:05 +0400

    fix(Label): change prop names in Label component

--- src/components/atoms/Label/Label.stories.tsx
+++ src/components/atoms/Label/Label.stories.tsx
@@ -10,18 +10,18 @@ const meta: Meta<ILabelProps> = {
     component: Label,
     argTypes: {
         size: args({ control: "select", ...propCategory.appearance }),
-        labelText: args({ control: "text", ...propCategory.content }),
+        text: args({ control: "text", ...propCategory.content }),
         required: args({ control: "boolean", ...propCategory.content }),
         infoText: args({ control: "text", ...propCategory.content }),
         disabled: args({ control: "boolean", ...propCategory.states }),
-        isLoading: args({ control: "boolean", ...propCategory.states }),
+        loading: args({ control: "boolean", ...propCategory.states }),
         className: args({ control: "false", ...propCategory.appearance }),
         children: args({ control: "false", ...propCategory.content }),
         readOnly: args({ control: "false", ...propCategory.states })
     },
     args: {
         size: "medium",
-        labelText: "label",
+        text: "label",
         required: false
     }
 };
