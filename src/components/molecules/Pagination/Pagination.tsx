ae65f069abf1b4a6a64cc16b9c9ca6afbf6e997e
abrahamyan.karen <abrahamyan.karen@softconstruct.com>
2025-04-30 16:55:22 +0400

    feat(Pagination): component UI

--- src/components/molecules/Pagination/Pagination.tsx
+++ src/components/molecules/Pagination/Pagination.tsx
@@ -1,6 +1,10 @@
 import React, { FC } from "react";
 import classNames from "classnames";
 
+import { ChevronDoubleLeft, ChevronDoubleRight, ChevronLeft, ChevronRight, ThreeDotsHorizontal } from "@geneui/icons";
+
+import Button from "@components/atoms/Button";
+
 // Styles
 import "./Pagination.scss";
 
@@ -14,7 +18,76 @@ interface IPaginationProps {
 }
 
 const Pagination: FC<IPaginationProps> = ({ className }) => {
-    return <div className={classNames("pagination", className)}>Pagination</div>;
+    const isRTLMode = document.dir === "rtl";
+
+    return (
+        <div className={classNames("pagination", className)}>
+            <div className="pagination__perpage">
+                {/* todo: import 'Dropdown' component */}
+                <div className="pagination__select">
+                    <select>
+                        <option value="5">5/Page</option>
+                        <option value="25">25/Page</option>
+                        <option value="50">50/Page</option>
+                    </select>
+                </div>
+                <div className="pagination__perpage_values">
+                    <span>1-25</span> of <span>755</span>
+                </div>
+            </div>
+            <div className="pagination__nav">
+                <div className="pagination__nav_list">
+                    <Button
+                        appearance="secondary"
+                        displayType="text"
+                        Icon={isRTLMode ? ChevronRight : ChevronLeft}
+                        onClick={() => {}}
+                    />
+                    <button className="pagination__nav_item" type="button">
+                        <span className="pagination__nav_value">1</span>
+                    </button>
+                    <Button appearance="secondary" displayType="text" Icon={ThreeDotsHorizontal} onClick={() => {}} />
+                    <button className="pagination__nav_item" type="button">
+                        <span className="pagination__nav_value">5</span>
+                    </button>
+                    {/* todo: add "pagination__nav_item_selected" className for selected state */}
+                    <button className="pagination__nav_item pagination__nav_item_selected" type="button">
+                        <span className="pagination__nav_value">6</span>
+                    </button>
+                    {/* todo: add "disabled" attribute for disabled state */}
+                    <button className="pagination__nav_item" type="button" disabled>
+                        <span className="pagination__nav_value">7</span>
+                    </button>
+                    <button className="pagination__nav_item" type="button">
+                        <span className="pagination__nav_value">8</span>
+                    </button>
+                    <Button
+                        appearance="secondary"
+                        displayType="text"
+                        Icon={isRTLMode ? ChevronDoubleLeft : ChevronDoubleRight}
+                        onClick={() => {}}
+                    />
+                    <button className="pagination__nav_item" type="button">
+                        <span className="pagination__nav_value">15</span>
+                    </button>
+                    <Button
+                        appearance="secondary"
+                        displayType="text"
+                        Icon={isRTLMode ? ChevronLeft : ChevronRight}
+                        onClick={() => {}}
+                    />
+                </div>
+                <div className="pagination__nav_specific">
+                    <span>Go to</span>
+                    {/* todo: import 'Text Field' component */}
+                    <div className="pagination__input">
+                        <input type="text" />
+                    </div>
+                    <span>Page</span>
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export { IPaginationProps, Pagination as default };
