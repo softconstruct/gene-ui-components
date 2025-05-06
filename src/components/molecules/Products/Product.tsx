b236f6c731c5593797cd5313d25938f65875735d
abrahamyan.karen <abrahamyan.karen@softconstruct.com>
2025-05-06 12:34:49 +0400

    fix(Products): bEM syntax for classNames

--- src/components/molecules/Products/Product.tsx
+++ src/components/molecules/Products/Product.tsx
@@ -49,10 +49,10 @@ const ProductButton: FC<Omit<IProductProps, "withBadge">> = (props) => {
             className={classNames("products__item", className, { products__item_disabled: disabled })}
             onClick={() => onClick(props)}
         >
-            <span className="products__item-logo">
+            <span className="products__item_logo">
                 <Icon size={48} />
             </span>
-            <Text as="span" className="products__item-title" alignment="center">
+            <Text as="span" className="products__item_title" alignment="center">
                 {title}
             </Text>
         </button>
