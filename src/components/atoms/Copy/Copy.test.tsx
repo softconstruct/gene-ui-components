51f5124e0fd03ed9136c8f619cfa4ca1b29a58fd
Galust <galust.gabrielyan@softconstruct.com>
2025-05-29 12:43:32 +0400

    feat(Copy): add tests

--- src/components/atoms/Copy/Copy.test.tsx
+++ src/components/atoms/Copy/Copy.test.tsx
@@ -25,11 +25,36 @@ describe("Copy Component", () => {
         expect(wrapper.exists()).toBeTruthy();
     });
 
+    it("renders className prop correctly", () => {
+        const className = "test-class";
+        const copyWrapper = wrapper.setProps({ className });
+
+        expect(copyWrapper.find(Button).hasClass(className)).toBeTruthy();
+    });
+
     it("renders the copy icon", () => {
         expect(wrapper.find(Button).props().Icon).toBe(Copy);
     });
 
-    it("renders the correct tooltip text", () => {
+    it("renders the correct copyTooltipText", () => {
         expect(wrapper.find("Tooltip").prop("text")).toBe(defaultProps.copyTooltipText);
+    });
+
+    it.each<ICopyProps["size"]>(["small", "medium", "large", "smallNudge"])("should have %s size", (size) => {
+        const copyWrapper = wrapper.setProps({ size });
+        expect(copyWrapper.find(Button).props().size).toBe(size);
+    });
+
+    it.each<ICopyProps["appearance"]>(["secondary", "primary", "inverse"])(
+        "should have %s appearance",
+        (appearance) => {
+            const copyWrapper = wrapper.setProps({ appearance });
+            expect(copyWrapper.find(Button).props().appearance).toBe(appearance);
+        }
+    );
+
+    it("renders disabled prop correctly", () => {
+        const copyWrapper = wrapper.setProps({ disabled: true });
+        expect(copyWrapper.find(Button).props().disabled).toBeTruthy();
     });
 });
