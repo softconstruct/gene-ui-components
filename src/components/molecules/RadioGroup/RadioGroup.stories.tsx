e21b16ed369f7bb7b3344e206d7a207aadf33aa0
abrahamyan.karen <abrahamyan.karen@softconstruct.com>
2025-07-07 16:44:50 +0400

    feat(RadioGroup): component UI

--- src/components/molecules/RadioGroup/RadioGroup.stories.tsx
+++ src/components/molecules/RadioGroup/RadioGroup.stories.tsx
@@ -1,24 +1,34 @@
-import { Meta, StoryObj } from "@storybook/react";
+import React, { FC } from "react";
+import { Meta } from "@storybook/react";
 
 // Helpers
 import { args, propCategory } from "../../../../stories/assets/storybook.globals";
 // Components
 import RadioGroup, { IRadioGroupProps } from "./index";
 
-const meta: Meta<IRadioGroupProps> = {
+const meta: Meta<typeof RadioGroup> = {
     title: "Molecules/RadioGroup",
     component: RadioGroup,
     argTypes: {
+        label: args({ control: "text", ...propCategory.content }),
+        disabled: args({ control: "boolean", ...propCategory.states }),
+        helperText: args({ control: "text", ...propCategory.content }),
+        readOnly: args({ control: "boolean", ...propCategory.states }),
+        type: args({ control: "select", ...propCategory.appearance }),
         className: args({ control: "false", ...propCategory.appearance })
         // fill RadioGroup component argTypes
     },
     args: {
+        label: "Group Label",
+        helperText: "Helper Text"
         // fill RadioGroup component args
     }
 };
 
 export default meta;
 
-type Story = StoryObj<IRadioGroupProps>;
+const Template: FC<IRadioGroupProps> = (props) => <RadioGroup {...props} />;
 
-export const Default: Story = {};
+export const Default = Template.bind({});
+
+Default.args = {} as IRadioGroupProps;
--- src/components/molecules/RadioGroup/RadioGroup.tsx
+++ src/components/molecules/RadioGroup/RadioGroup.tsx
@@ -1,10 +1,39 @@
 import React, { FC } from "react";
 import classNames from "classnames";
 
+import HelperText from "@components/atoms/HelperText";
+import Label from "@components/atoms/Label";
+import Radio from "@components/atoms/Radio";
+
 // Styles
 import "./RadioGroup.scss";
 
 interface IRadioGroupProps {
+    /**
+     *  The text displayed as the label for the checkbox group, describing its purpose or function.
+     */
+    label?: string;
+    /**
+     *  Specifies whether the checkbox group is mandatory for completing a form.
+     */
+    required?: boolean;
+    /**
+     *  Disables the checkbox group, preventing it from being interacted with.
+     */
+    disabled?: boolean;
+    /**
+     *  Displays the checkbox group as read-only, where users cannot modify its value.
+     */
+    readOnly?: boolean;
+    /**
+     *  Helper text to provide context or explain any errors related to the checkbox group.
+     */
+    helperText?: string;
+    /**
+     *  Determines the checkbox groups appearance based on its status.<br>
+     *  Possible values: `rest | error`
+     */
+    type?: "rest" | "error";
     /**
      * Additional class for the parent element.
      * This prop should be used to set placement properties for the element relative to its parent using BEM conventions.
@@ -16,8 +45,31 @@ interface IRadioGroupProps {
 /**
  * A radio group allows users to select one option from a group of related choices. Each radio in the group is accompanied by a label, and only one radio within the group can be selected at any time.
  */
-const RadioGroup: FC<IRadioGroupProps> = ({ className }) => {
-    return <div className={classNames("radioGroup", className)}>RadioGroup</div>;
+const RadioGroup: FC<IRadioGroupProps> = ({
+    label,
+    required,
+    disabled,
+    readOnly,
+    helperText,
+    type = "rest" as const,
+    className
+}) => {
+    return (
+        <div className={classNames("radioGroup", className)}>
+            <Label text={label} required={required} disabled={disabled} readOnly={readOnly} />
+
+            <Radio label="Label" type={type} value="" name="name" disabled={disabled} readOnly={readOnly} />
+            <Radio label="Label" type={type} value="" name="name" disabled={disabled} readOnly={readOnly} />
+            <Radio label="Label" type={type} value="" name="name" disabled={disabled} readOnly={readOnly} />
+            <Radio label="Label" type={type} value="" name="name" disabled={disabled} readOnly={readOnly} />
+
+            {helperText && (
+                <div className="checkbox__infoContainer">
+                    <HelperText text={helperText} disabled={disabled} type={type} />
+                </div>
+            )}
+        </div>
+    );
 };
 
 export { IRadioGroupProps, RadioGroup as default };
