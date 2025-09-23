f55ea345d6595bfed9b63a1b000156bf06ada9d0
Galust <galust.gabrielyan@softconstruct.com>
2025-09-23 14:13:09 +0400

    fix(TagGroup): add renderToggleText prop

--- src/components/molecules/TagGroup/TagGroup.tsx
+++ src/components/molecules/TagGroup/TagGroup.tsx
@@ -1,4 +1,4 @@
-import React, { Children, cloneElement, createContext, FC, isValidElement, ReactNode, useMemo, useState } from "react";
+import React, { Children, cloneElement, FC, isValidElement, ReactNode, useMemo, useState } from "react";
 import classNames from "classnames";
 
 // Icons
@@ -17,42 +17,32 @@ import "./TagGroup.scss";
 // Custom hooks
 import { useTagVisibility } from "./useTagVisibility";
 
-interface ITagGroupContextProps {
-    /**
-     * Size
-     * Possible values: `medium | small`;
-     */
-    size?: ITagProps["size"];
-}
-
-interface ITagGroupProps extends ITagGroupContextProps {
+interface ITagGroupProps {
     /**
      * Additional class for the parent element.
      * This prop should be used to set placement properties for the element relative to its parent using BEM conventions.
      */
     className?: string;
     /**
+     * Size for all `<Tag/>` components within the `<TagGroup/>`.<br/>
+     * Possible values: `medium | small`;
+     */
+    size?: ITagProps["size"];
+    /**
      * Provide `<Tag/>` components to be rendered in the `<TagGroup/>`
      */
     children: ReactNode;
     /**
-     * Text to display on the toggle button when collapsed.
-     * If not provided, only the icon will be shown
+     * A function to dynamically generate the toggle button's text.
+     * It receives the `expanded` state and returns the string to display.
      */
-    showMoreText?: string;
-    /**
-     * Text to display on the toggle button when expanded.
-     * If not provided, only the icon will be shown
-     */
-    showLessText?: string;
+    renderToggleText?: (expanded: boolean) => string;
 }
 
 /**
  * Tag Group displays a list of selected options, offering a clear and organized way to present chosen items.
  */
-export const TagGroupContext = createContext<ITagGroupContextProps>({});
-
-const TagGroup: FC<ITagGroupProps> = ({ className, children, size = "medium", showMoreText, showLessText }) => {
+const TagGroup: FC<ITagGroupProps> = ({ className, children, size = "medium", renderToggleText }) => {
     const [isExpanded, setIsExpanded] = useState(false);
     const childrenArray = useMemo(() => Children.toArray(children), [children]);
     const { width } = useWindowSize();
@@ -65,7 +55,7 @@ const TagGroup: FC<ITagGroupProps> = ({ className, children, size = "medium", sh
 
     const renderChildren = (childrenToRender: ReactNode[]) => {
         return childrenToRender.map((child) =>
-            isValidElement(child) ? cloneElement(child, { ...child.props, size: size as ITagProps["size"] }) : child
+            isValidElement(child) ? cloneElement(child, { ...child.props, size }) : child
         );
     };
 
@@ -73,39 +63,33 @@ const TagGroup: FC<ITagGroupProps> = ({ className, children, size = "medium", sh
         setIsExpanded((prevExpanded) => !prevExpanded);
     };
 
-    const contextValue = useMemo(() => ({ size }), [size]);
-
     return (
-        <TagGroupContext.Provider value={contextValue}>
-            <div className={classNames("tagGroup", className)}>
-                <div className={classNames("tagGroup__container", { tagGroup__container_expanded: isExpanded })}>
-                    <div
-                        ref={containerRef}
-                        className={classNames("tagGroup__tags", {
-                            [`tagGroup__tags_size_${size}`]: !isExpanded
-                        })}
-                    >
-                        {renderChildren(
-                            isExpanded ? childrenArray : childrenArray.slice(0, tagVisibility.visibleCount)
-                        )}
-                    </div>
-                    {tagVisibility.shouldShowToggleButton && (
-                        <Button
-                            className="tagGroup__showButton"
-                            appearance="secondary"
-                            size={size}
-                            layout="text"
-                            iconPosition="after"
-                            Icon={isExpanded ? ChevronUp : ChevronDown}
-                            onClick={handleToggleExpanded}
-                        >
-                            {isExpanded ? showLessText : showMoreText}
-                        </Button>
-                    )}
+        <div className={classNames("tagGroup", className)}>
+            <div className={classNames("tagGroup__container", { tagGroup__container_expanded: isExpanded })}>
+                <div
+                    ref={containerRef}
+                    className={classNames("tagGroup__tags", {
+                        [`tagGroup__tags_size_${size}`]: !isExpanded
+                    })}
+                >
+                    {renderChildren(isExpanded ? childrenArray : childrenArray.slice(0, tagVisibility.visibleCount))}
                 </div>
+                {tagVisibility.shouldShowToggleButton && (
+                    <Button
+                        className="tagGroup__showButton"
+                        appearance="secondary"
+                        size={size}
+                        layout="text"
+                        iconPosition="after"
+                        Icon={isExpanded ? ChevronUp : ChevronDown}
+                        onClick={handleToggleExpanded}
+                    >
+                        {renderToggleText?.(isExpanded) || ""}
+                    </Button>
+                )}
             </div>
-        </TagGroupContext.Provider>
+        </div>
     );
 };
 
-export { ITagGroupProps, ITagGroupContextProps, TagGroup as default };
+export { ITagGroupProps, TagGroup as default };
