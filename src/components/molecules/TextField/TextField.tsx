58ff09c2f2ac292b1b39d8a1bd4773d716981935
abrahamyan.karen <abrahamyan.karen@softconstruct.com>
2025-11-13 13:18:14 +0400

    fix(TextField): placeholder color, label showing condition

--- src/components/molecules/TextField/TextField.tsx
+++ src/components/molecules/TextField/TextField.tsx
@@ -275,16 +275,18 @@ const TextField = forwardRef<ITextFieldRef, ITextFieldProps>(
 
         return (
             <div className={classNames("textField", className)}>
-                <Label
-                    text={label}
-                    required={required}
-                    className="textField__label"
-                    disabled={disabled}
-                    infoText={infoText}
-                    readOnly={readOnly}
-                    labelFor={generatedId}
-                    size={labelSize}
-                />
+                {label && (
+                    <Label
+                        text={label}
+                        required={required}
+                        className="textField__label"
+                        disabled={disabled}
+                        infoText={infoText}
+                        readOnly={readOnly}
+                        labelFor={generatedId}
+                        size={labelSize}
+                    />
+                )}
                 <div
                     className={classNames(`textField__wrapper textField__wrapper_size_${size}`, paddingClassesForIcon, {
                         textField__wrapper_readOnly: readOnly && !disabled,
