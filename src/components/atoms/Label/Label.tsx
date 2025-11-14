2d1c2448f82b5c2990e7f6914ee7efb8e17f36c2
Galust <galust.gabrielyan@softconstruct.com>
2025-11-14 17:30:00 +0400

    fix(Label): remove aria-label for non text div

--- src/components/atoms/Label/Label.tsx
+++ src/components/atoms/Label/Label.tsx
@@ -161,7 +161,6 @@ const Label: FC<ILabelProps> = ({
                 className
             )}
             {...(labelFor && { htmlFor: labelFor })}
-            aria-label={text}
             onClick={handlePreventLabelInteraction}
         >
             {children}
