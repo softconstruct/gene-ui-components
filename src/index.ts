32c271f5ca57b9b202659dc9f451a0baebbedb6d
Galust <galust.gabrielyan@softconstruct.com>
2025-11-20 14:28:47 +0400

    fix(Radio): export reorder

--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,6 @@ export { default as Info, IInfoProps } from "./components/atoms/Info";
 export { default as Button, IButtonProps } from "./components/atoms/Button";
 export { default as Logo, ILogoProps } from "./components/atoms/Logo";
 export { default as Rate, IRateProps } from "./components/atoms/Rate";
-export { default as Radio, IRadioProps } from "@components/molecules/Radio";
 export { default as Text, ITextProps } from "./components/atoms/Text";
 export { default as TextLink, ITextLinkProps } from "./components/atoms/TextLink";
 export { Popover, PopoverBody, PopoverFooter, PopoverFooterActions, IPopoverProps } from "./components/atoms/Popover";
@@ -42,6 +41,7 @@ export { KeyValue, Key, Value, IKeyValueProps, IKeyProps, IValueProps } from "./
 export { Menu, MenuItem, IMenuProps, IMenuItemProps } from "./components/molecules/Menu";
 export { default as Navigation, INavigationProps, INavigationData } from "./components/molecules/Navigation";
 export { default as Checkbox, ICheckboxProps } from "./components/molecules/Checkbox";
+export { default as Radio, IRadioProps } from "@components/molecules/Radio";
 export { default as Profile, IProfileProps, IProfileData } from "./components/molecules/Profile";
 export { default as Pagination, IPaginationProps } from "./components/molecules/Pagination";
 export { default as TextField, ITextFieldProps } from "./components/molecules/TextField";
