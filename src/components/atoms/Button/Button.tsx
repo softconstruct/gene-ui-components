180a1a05bf97bd5382a2aee0a8988e3399bcb424
Hayk Kirakosyan <kirakosyan.hayk@softconstruct.com>
2025-11-24 14:48:23 +0400

    feat(Button): added a new id prop for button component

--- src/components/atoms/Button/Button.tsx
+++ src/components/atoms/Button/Button.tsx
@@ -103,6 +103,11 @@ interface IButtonProps {
      * @default 0
      */
     tabIndex?: number;
+    /**
+     * A unique identifier for the button element.
+     * Useful for accessibility purposes, like `aria-labelledby`.
+     */
+    id?: string;
 }
 
 const loadingTypes = {
@@ -124,6 +129,7 @@ const Button = forwardRef<HTMLButtonElement, IButtonProps>(
             disabled,
             fullWidth,
             name,
+            id,
             size = "medium",
             layout = "fill",
             children,
@@ -149,6 +155,7 @@ const Button = forwardRef<HTMLButtonElement, IButtonProps>(
             <button
                 ref={ref}
                 name={name}
+                id={id}
                 onFocus={onFocus}
                 onBlur={onBlur}
                 // eslint-disable-next-line react/button-has-type
