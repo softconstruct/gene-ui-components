1529ff2a66a27b6b910b820410122ac6ce78d97e
Galust <galust.gabrielyan@softconstruct.com>
2025-12-15 14:33:41 +0400

    fix(Accordion): story

--- src/components/molecules/Accordion/Accordion.stories.tsx
+++ src/components/molecules/Accordion/Accordion.stories.tsx
@@ -1,58 +1,17 @@
 import React from "react";
 import { Meta, StoryObj } from "@storybook/react";
 
-import { Download, Globe, Magnifier, RecycleBin, Tag } from "@geneui/icons";
+import { Download, Globe, RecycleBin, Tag } from "@geneui/icons";
 
 // Components
+import Avatar from "@components/atoms/Avatar";
+import Button from "@components/atoms/Button";
+import Divider from "@components/atoms/Divider";
+import Text from "@components/atoms/Text";
 import { Accordion, AccordionItem, IAccordionItemProps, IAccordionProps } from "@components/molecules/Accordion";
 
 // Helpers
 import { args, propCategory, storyObjBuilder } from "../../../../stories/assets/storybook.globals";
-
-const AccordionItemContent = (
-    <div
-        style={{
-            display: "flex",
-            flexDirection: "column",
-            justifyContent: "center",
-            alignItems: "center",
-            width: "100%",
-            height: "100%",
-            padding: "var(--guit-ref-spacing-2xsmall)",
-            backgroundColor: "#F4E1EC",
-            border: "var(--guit-ref-border-width-thin) solid var(--guit-sem-color-border-accent-red)"
-        }}
-    >
-        <div
-            style={{
-                display: "flex",
-                flexDirection: "column",
-                justifyContent: "center",
-                alignItems: "center",
-                color: "var(--guit-ref-color-magenta-500base)"
-            }}
-        >
-            <Magnifier style={{ marginBottom: "var(--guit-ref-spacing-2xsmall)" }} color="#A60063" />
-            <span
-                style={{
-                    marginTop: "var(--guit-ref-spacing-2xsmall)",
-                    fontSize: "2rem"
-                }}
-            >
-                Slot component
-            </span>
-            <span
-                style={{
-                    marginTop: "var(--guit-ref-spacing-3xsmall)",
-                    fontSize: "1.4rem",
-                    textAlign: "center"
-                }}
-            >
-                Replace it with any component using the &quot;Component Instance&quot; swapper.
-            </span>
-        </div>
-    </div>
-);
 
 const meta: Meta<IAccordionProps> = {
     title: "Molecules/Accordion",
@@ -92,7 +51,20 @@ export const Default: Story = {
                         }
                     ]}
                 >
-                    {AccordionItemContent}
+                    <Text as="p">
+                        Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of
+                        classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a
+                        Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin
+                        words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in
+                        classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32
+                        and 1.10.33 of de Finibus Bonorum et Malorum (The Extremes of Good and Evil) by Cicero, written
+                        in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance.
+                        The first line of Lorem Ipsum, Lorem ipsum dolor sit amet.., comes from a line in section
+                        1.10.32. The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those
+                        interested. Sections 1.10.32 and 1.10.33 from de Finibus Bonorum et Malorum by Cicero are also
+                        reproduced in their exact original form, accompanied by English versions from the 1914
+                        translation by H. Rackham.
+                    </Text>
                 </AccordionItem>
                 <AccordionItem
                     title="Accordion Item 2"
@@ -109,7 +81,20 @@ export const Default: Story = {
                         }
                     ]}
                 >
-                    {AccordionItemContent}
+                    <div style={{ display: "flex", flexDirection: "column", gap: "16px" }}>
+                        <Text as="h3" variant="labelMediumMedium" alignment="center">
+                            Test content
+                        </Text>
+                        <Divider
+                            text="GeneUI divider"
+                            Icon={Globe}
+                            content={<Avatar fullName="Test Name" onClick={() => {}} />}
+                            contentPosition="before"
+                        />
+                        <Button size="medium" fullWidth>
+                            test button
+                        </Button>
+                    </div>
                 </AccordionItem>
             </Accordion>
         );
@@ -128,7 +113,7 @@ export const AccordionItemStory: StoryItem = storyObjBuilder({
     args: {
         title: "Accordion Item",
         Icon: Tag,
-        children: AccordionItemContent
+        children: "Accordion Content"
     },
     render: (props) => {
         return (
