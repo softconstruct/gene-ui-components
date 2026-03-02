c512ee493e1b4efa67b95908a248f8f328ee9fdc
Areg Hakobyan <areghakobyan@CCMP022348s-MacBook-Pro.local>
2026-03-02 15:20:02 +0400

    fix(Pagination): fix pagination per page values styles

--- src/components/molecules/Pagination/Pagination.test.tsx
+++ src/components/molecules/Pagination/Pagination.test.tsx
@@ -101,7 +101,7 @@ describe("Pagination", () => {
         // Check if the text indicating the page range contains the custom label.
         const perPageValuesText = setup.find(".pagination__perpage_values").text();
         expect(perPageValuesText).toContain(customLabel);
-        // Example assertion: "10 of 20"
+        // Example assertion: "10 of 10"
         expect(perPageValuesText).toBe(`10 ${customLabel} 10`);
     });
 
