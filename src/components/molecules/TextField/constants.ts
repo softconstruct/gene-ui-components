4474da37d1663e7284f327baaa804b2595134957
Galust <galust.gabrielyan@softconstruct.com>
2026-03-05 13:51:22 +0400

    fix(TextField): size mapping reduce

--- src/components/molecules/TextField/constants.ts
+++ src/components/molecules/TextField/constants.ts
@@ -1,26 +1,23 @@
 // Size mapping objects for Label, Button, IconBefore/IconAfter and HelperText components
-const labelSizeMap = {
-    large: "medium" as const,
-    medium: "medium" as const,
-    small: "small" as const
-};
 
-const helperTextSizeMap = {
-    large: "medium" as const,
-    medium: "medium" as const,
-    small: "small" as const
-};
+const textSizeMap = {
+    large: "medium",
+    medium: "medium",
+    small: "small"
+} as const;
+
+const labelSizeMap = textSizeMap;
+const helperTextSizeMap = textSizeMap;
 
 const iconSizeMap = {
-    large: 24 as const,
-    medium: 24 as const,
-    small: 20 as const
-};
+    large: 24,
+    medium: 24,
+    small: 20
+} as const;
 
 const actionButtonSizeMap = {
-    large: "small" as const,
-    medium: "small" as const,
-    small: "smallNudge" as const
-};
-
+    large: "small",
+    medium: "small",
+    small: "smallNudge"
+} as const;
 export { labelSizeMap, helperTextSizeMap, iconSizeMap, actionButtonSizeMap };
