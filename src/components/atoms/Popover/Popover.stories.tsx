49d01c4cfc5ae8fd9cf740e17003800ad043af4f
Hayk Kirakosyan <kirakosyan.hayk@softconstruct.com>
2026-03-05 16:33:42 +0400

    fix(Popover): change story content to loremIpsum

--- src/components/atoms/Popover/Popover.stories.tsx
+++ src/components/atoms/Popover/Popover.stories.tsx
@@ -109,7 +109,17 @@ const WithoutFooterComponent: FC<IPopoverProps> = (props) => {
         <div style={{ margin: "500px 500px", height: 1000 }}>
             <Popover {...props} setProps={setPropsForContent}>
                 <PopoverBody>
-                    <div className="swapComponent" style={{ minHeight: "100%", background: "#F4E1EC" }} />
+                    <span>
+                        Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of
+                        classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a
+                        Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin
+                        words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in
+                        classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32
+                        and 1.10.33 of de Finibus Bonorum et Malorum (The Extremes of Good and Evil) by Cicero, written
+                        in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance.
+                        The first line of Lorem Ipsum, Lorem ipsum dolor sit amet.., comes from a line in section
+                        1.10.32.
+                    </span>
                 </PopoverBody>
             </Popover>
             <Button onClick={() => {}} {...propsForContent}>
@@ -132,7 +142,17 @@ const WithoutHeaderComponent: FC<IPopoverProps> = (props) => {
         <div style={{ margin: "500px 500px", height: 1000 }}>
             <Popover {...props} setProps={setPropsForContent} title="">
                 <PopoverBody>
-                    <div className="swapComponent" style={{ minHeight: "100%", background: "#F4E1EC" }} />
+                    <span>
+                        Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of
+                        classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a
+                        Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin
+                        words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in
+                        classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32
+                        and 1.10.33 of de Finibus Bonorum et Malorum (The Extremes of Good and Evil) by Cicero, written
+                        in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance.
+                        The first line of Lorem Ipsum, Lorem ipsum dolor sit amet.., comes from a line in section
+                        1.10.32.
+                    </span>
                 </PopoverBody>
                 <PopoverFooter
                     actions={[
