e3848e5e4e1e5a8db4a3fcd4a885d3665a152ed3
Hayk Kirakosyan <kirakosyan.hayk@softconstruct.com>
2026-03-12 12:18:53 +0400

    feat(Popover): add defaultOpen for default story and remove 2 of them

--- src/components/atoms/Popover/Popover.stories.tsx
+++ src/components/atoms/Popover/Popover.stories.tsx
@@ -1,8 +1,6 @@
 import React, { ComponentType, FC, useRef, useState } from "react";
 import { Meta, StoryObj } from "@storybook/react";
 
-import { Info } from "@geneui/icons";
-
 // Helpers
 import { args, propCategory } from "../../../../stories/assets/storybook.globals";
 // Components
@@ -98,86 +96,10 @@ const PopoverStoryComponent: FC<IPopoverProps> = (props) => {
 };
 
 export const Default: Story = {
-    render: (props: IPopoverProps) => <PopoverStoryComponent {...props} />
-};
-
-const WithoutFooterComponent: FC<IPopoverProps> = (props) => {
-    const [propsForContent, setPropsForContent] = useState({});
-    return (
-        <div style={{ margin: "500px 500px", height: 1000 }}>
-            <Popover {...props} setProps={setPropsForContent}>
-                <PopoverBody>
-                    <span>
-                        Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of
-                        classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a
-                        Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin
-                        words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in
-                        classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32
-                        and 1.10.33 of de Finibus Bonorum et Malorum (The Extremes of Good and Evil) by Cicero, written
-                        in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance.
-                        The first line of Lorem Ipsum, Lorem ipsum dolor sit amet.., comes from a line in section
-                        1.10.32.
-                    </span>
-                </PopoverBody>
-            </Popover>
-            <Button onClick={() => {}} {...propsForContent}>
-                Click for open
-            </Button>
-        </div>
-    );
-};
-
-export const WithoutFooter: Story = {
-    render: (props: IPopoverProps) => <WithoutFooterComponent {...props} />,
+    render: (props: IPopoverProps) => <PopoverStoryComponent {...props} />,
     args: {
-        Icon: Info
+        defaultOpen: true
     }
-};
-
-const WithoutHeaderComponent: FC<IPopoverProps> = (props) => {
-    const [propsForContent, setPropsForContent] = useState({});
-    return (
-        <div style={{ margin: "500px 500px", height: 1000 }}>
-            <Popover {...props} setProps={setPropsForContent} title="">
-                <PopoverBody>
-                    <span>
-                        Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of
-                        classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a
-                        Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin
-                        words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in
-                        classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32
-                        and 1.10.33 of de Finibus Bonorum et Malorum (The Extremes of Good and Evil) by Cicero, written
-                        in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance.
-                        The first line of Lorem Ipsum, Lorem ipsum dolor sit amet.., comes from a line in section
-                        1.10.32.
-                    </span>
-                </PopoverBody>
-                <PopoverFooter
-                    actions={[
-                        {
-                            text: "Primary",
-                            appearance: "inverse",
-                            onClick: () => {}
-                        },
-                        {
-                            text: "Secondary",
-                            appearance: "primary",
-                            onClick: () => {}
-                        }
-                    ]}
-                >
-                    <Avatar size="small" fullName="Jane Smith" color="purple" />
-                </PopoverFooter>
-            </Popover>
-            <Button onClick={() => {}} {...propsForContent}>
-                Click for open
-            </Button>
-        </div>
-    );
-};
-
-export const WithoutHeader: Story = {
-    render: (props: IPopoverProps) => <WithoutHeaderComponent {...props} />
 };
 
 const WithoutHeaderAndFooterComponent: FC<IPopoverProps> = (props) => {
