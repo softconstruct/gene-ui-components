780708e73a03251ef9a469adcb8a968b786b2c70
Sos Petrosyan <sos.petrosyan@softconstruct.com>
2026-03-16 18:51:04 +0400

    feat(OtpField): component UI

--- src/components/molecules/OTPField/OTPField.tsx
+++ src/components/molecules/OTPField/OTPField.tsx
@@ -1,6 +1,11 @@
 import React, { FC } from "react";
 import classNames from "classnames";
 
+import HelperText from "@components/atoms/HelperText";
+import Text from "@components/atoms/Text";
+import Notification from "@components/molecules/Notification";
+import TextField from "@components/molecules/TextField";
+
 // Styles
 import "./OTPField.scss";
 
@@ -17,7 +22,28 @@ interface IOTPFieldProps {
  * The OTP Field component is an input field designed for entering a one-time password, often used for multi-factor authentication. The OTP Field component ensures that applications can securely verify user actions and identities, thereby protecting against unauthorized access and enhancing overall security.
  */
 const OTPField: FC<IOTPFieldProps> = ({ className }) => {
-    return <div className={classNames("oTPField", className)}>OTPField</div>;
+    return (
+        <div className={classNames("otpField otpField_size_large", className)}>
+            {/* otpField_size_large / otpField_size_medium */}
+            <div className="otpField__wrapper">
+                <div className="otpField__textFieldWrapper">
+                    <TextField className="otpField__textField" size="large" type="number" />
+                    <TextField className="otpField__textField" size="large" type="number" />
+                    <TextField className="otpField__textField" size="large" type="number" />
+                    <TextField className="otpField__textField" size="large" type="number" />
+                    <TextField className="otpField__textField" size="large" type="number" />
+                    <TextField className="otpField__textField" size="large" type="number" />
+                </div>
+                <div className="otpField__content">
+                    <HelperText className="otpField__helperText" text="Code is valid for" />
+                    <Text className="otpField__timer" as="span" variant="bodyMediumMedium">
+                        02:32
+                    </Text>
+                </div>
+            </div>
+            <Notification variant="sectionMessage" open status="error" title="The entered code is invalid." />
+        </div>
+    );
 };
 
 export { IOTPFieldProps, OTPField as default };
