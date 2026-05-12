694f821d49ea0d88b5010388470414f3ef0ace6e
Galust <galust.gabrielyan@softconstruct.com>
2026-05-12 12:50:02 +0400

    fix(List): interface export

--- src/index.ts
+++ src/index.ts
@@ -87,7 +87,7 @@ export type {
     IActionableListTexts
 } from "./components/molecules/ActionableList";
 export { default as ActionableList } from "./components/molecules/ActionableList";
-export { default as List, IListProps } from "./components/molecules/List";
+export { default as List, IListProps, IListItemData } from "./components/molecules/List";
 
 // Organisms
 export {
