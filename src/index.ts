9f7798ee16768bf9fedb8df9b4e154a0b80eadbd
Galust <galust.gabrielyan@softconstruct.com>
2026-07-01 18:26:05 +0400

    fix(ImagePreview): interface export

--- src/index.ts
+++ src/index.ts
@@ -97,7 +97,7 @@ export {
     IActionableListTexts
 } from "./components/molecules/ActionableList";
 export { default as List, IListProps, IListItemData } from "./components/molecules/List";
-export { default as ImagePreview } from "./components/molecules/ImagePreview";
+export { default as ImagePreview, IImagePreviewImage, IImagePreviewProps } from "./components/molecules/ImagePreview";
 
 // Organisms
 export {
