120e38d72b5f3503861ac6d8567fefc76efb014b
Albert Hovhannisyan <hovhannisyan.albert@softconstruct.local>
2026-07-02 13:30:17 +0400

    fix(TimePicker): tests

--- src/components/molecules/TimePicker/TimePicker.stories.tsx
+++ src/components/molecules/TimePicker/TimePicker.stories.tsx
@@ -1,6 +1,5 @@
 import React, { ReactNode } from "react";
 import { Meta, StoryObj } from "@storybook/react";
-import { fn } from "@storybook/test";
 
 // Helpers
 import { args, propCategory } from "../../../../stories/assets/storybook.globals";
@@ -38,7 +37,7 @@ const meta: Meta<typeof TimePicker> = {
     args: {
         label: "Choose time",
         placeholder: "Select time",
-        onPopoverToggle: fn()
+        onPopoverToggle: jest.fn()
     }
 };
 
